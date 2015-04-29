7a09cc36fed8f6e8cf55a57e3c9f3f28010172bf
Boudewijn <Boudewijn@vangroos.nl>
2015-04-29 11:58:52 +0200

    Started creating a test for the DataReader.

---
+++ src/test/java/model/reader/DataReaderTest.java
@@ -0,0 +1,17 @@
+package model.reader;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Boudewijn on 29-4-2015.
+ */
+public class DataReaderTest extends TestCase {
+
+	@Test
+	public void testReadData() throws Exception {
+		
+	}
+}
