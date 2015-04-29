afd525f2e19cc1d47282e979e99e2cc103a05991
jenslangerak <J.Langerak@student.tudelft.nl>
2015-04-29 11:47:30 +0200

    classes for the dataModel

---
+++ src/main/java/model/dataModel/DataColumn.java
@@ -0,0 +1,9 @@
+package model.dataModel;
+
+/**
+ * Created by jens on 4/29/15.
+ */
+public class DataColumn {
+    DataValue type;
+    String name;
+}
---
+++ src/main/java/model/dataModel/DataValue.java
@@ -0,0 +1,8 @@
+package model.dataModel;
+import java.util.HashMap;
+
+/**
+ * Created by jens on 4/29/15.
+ */
+public class DataValue {
+}
