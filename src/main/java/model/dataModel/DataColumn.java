356eab351491ee5d725c107cd65703daf15025b1
jenslangerak <J.Langerak@student.tudelft.nl>
2015-04-29 18:04:11 +0200

    add javadoc

--- src/main/java/model/dataModel/DataColumn.java
+++ src/main/java/model/dataModel/DataColumn.java
@@ -1,21 +1,34 @@
 package model.dataModel;
 
 /**
- * Created by jens on 4/29/15.
+ * Class that specified what the data in a column is
  */
 public class DataColumn {
     DataValue type;
     String name;
 
+    /**
+     * Create a column
+     * @param t type of the column
+     * @param n name of the column
+     */
     public DataColumn(DataValue t, String n) {
         type = t;
         name = n;
     }
 
+    /**
+     * get the name of the column
+     * @return the name of the column
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * get the type of the column
+     * @return the type of the column
+     */
     public DataValue getType() {
         return type;
     }
--- src/main/java/model/dataModel/DataRow.java
+++ src/main/java/model/dataModel/DataRow.java
@@ -1,34 +1,61 @@
 package model.dataModel;
 import Exceptions.*;
 import java.util.HashMap;
+import java.util.logging.Logger;
 
 /**
- * Created by jens on 4/29/15.
+ * Class that represents a row of data
  */
 public class DataRow {
+    private Logger log = Logger.getLogger("DataRow");
+
     protected HashMap<DataColumn, DataValue> values;
 
+    /**
+     * create an empty row
+     */
     public DataRow() {
         values = new HashMap<DataColumn, DataValue>();
     }
 
+    /**
+     * create a row and set for the columns c, the values v
+     * @param c columns that the row should have
+     * @param v calue of the columns
+     * @throws ColumnValueMismatchException thrown when the number of columns is not equal to the number of values
+     * @throws ColumnValueTypeMismatchEception thrown when the value has a different type from what the columns expects
+     */
     public DataRow(DataColumn[] c, DataValue[] v) throws ColumnValueMismatchException, ColumnValueTypeMismatchEception{
         if(c.length != v.length) {
-            throw new ColumnValueMismatchException("Number of columns is not equal t the number of values");
+            ColumnValueMismatchException e = new ColumnValueMismatchException("Number of columns is not equal t the number of values");
+            log.throwing(this.getClass().getSimpleName(), "constructor", e);
+            throw e;
         }
         for(int i = 0; i < c.length; i ++) {
             if(c[i].getType().getClass().isInstance(v[i])){
                 values.put(c[i], v[i]);
             } else {
-                throw new ColumnValueTypeMismatchEception("Type of value is not a subtype of column tpye");
+                ColumnValueTypeMismatchEception e = new ColumnValueTypeMismatchEception("Type of value is not a subtype of column type");
+                log.throwing(this.getClass().getSimpleName(), "constructor", e);
+                throw e;
             }
         }
     }
 
+    /**
+     * Add the column with the value to the row
+     * @param column thw column where the value belongs to
+     * @param v the value of the added column
+     */
     public void setValue(DataColumn column, DataValue v) {
         values.put(column, v);
     }
 
+    /**
+     * get the value of a column
+     * @param c the column where you want the value from
+     * @return
+     */
     public DataValue getValue(DataColumn c) {
         return values.get(c);
     }
