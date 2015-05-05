973fa1bddd3247dfced65e20ee181ac25cfec192
Paul <P.J.vanWijk@student.tudelft.nl>
2015-05-05 16:55:48 +0000

    checkstyle fixes

--- src/main/java/xml/DataFile.java
+++ src/main/java/xml/DataFile.java
@@ -11,14 +11,23 @@ public class DataFile extends File {
 
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * The type of the datafile.
+	 */
 	private String type;
 	
-	public DataFile(String pathname, String type) {
+	/**
+	 * Creates a new datafile.
+	 * @param pathname the path to the file.
+	 * @param t the type of the datafile.
+	 */
+	public DataFile(String pathname, String t) {
 		super(pathname);
-		this.type = type;
+		this.type = t;
 	}
 
 	/**
+	 * returns the type of the datafile.
 	 * @return the type
 	 */
 	public String getType() {
@@ -26,12 +35,17 @@ public class DataFile extends File {
 	}
 
 	/**
-	 * @param type the type to set
+	 * @param t the type to set
 	 */
-	public void setType(String type) {
-		this.type = type;
+	public void setType(String t) {
+		this.type = t;
 	}
 	
+	/**
+	 * returns a string representation of the datafile.
+	 * @return the string representing the file.
+	 */
+	@Override
 	public String toString() {
 		return "[" + super.toString() + ", type=" + type + "]";
 	}
