42b19b79eb72caa0488ef831c3da01c0cab45311
Boudewijn <boudewijn@vangroos.nl>
2015-05-05 20:46:01 +0200

    Implemented the ConstraintsAnalysis

--- src/main/java/model/data/process/analysis/ConstraintAnalysis.java
+++ src/main/java/model/data/process/analysis/ConstraintAnalysis.java
@@ -1,7 +1,36 @@
 package model.data.process.analysis;
 
+import model.data.DataModel;
+import model.data.DataRow;
+import model.data.process.analysis.constraints.Constraint;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
+ * Implements the constraints analysis.
+ *
  * Created by Boudewijn on 5-5-2015.
  */
-public class ConstraintAnalysis {
+public class ConstraintAnalysis extends DataAnalysis {
+
+	private final Constraint constraint;
+
+	public ConstraintAnalysis(Constraint constraint) {
+		this.constraint = constraint;
+	}
+
+	@Override
+	public DataModel analyse(DataModel input) {
+		List<DataRow> out = new ArrayList<>();
+		Iterator<DataRow> iterator = input.getRows();
+		while (iterator.hasNext()) {
+			DataRow row = iterator.next();
+			if (constraint.check(row)) {
+				out.add(row);
+			}
+		}
+		return new DataModel(out, new ArrayList<>(input.getColumns().values()));
+	}
 }
