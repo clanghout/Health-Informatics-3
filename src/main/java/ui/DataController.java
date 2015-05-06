b0669b96be825e9ec7689a5bef7d88bde9a187e8
Boudewijn <Boudewijn@vangroos.nl>
2015-05-06 16:46:27 +0200

    Added an analyse button

--- src/main/java/ui/DataController.java
+++ src/main/java/ui/DataController.java
@@ -7,8 +7,20 @@ import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import model.data.DataModel;
+import model.data.process.analysis.ConstraintAnalysis;
+import model.data.process.analysis.DataAnalysis;
+import model.data.process.analysis.constraints.Constraint;
+import model.data.process.analysis.constraints.EqualityCheck;
+import model.data.value.StringValue;
+import model.reader.DataReader;
+import output.DataModelWriter;
+import xml.DataFile;
+import xml.XmlReader;
 
 import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * The controller for the Data tab
@@ -19,8 +31,12 @@ public class DataController {
 	@FXML private TextField fileNameField;
 	@FXML private Parent root;
 
+	private Logger logger = Logger.getLogger("DataController");
 
-	@FXML protected void handleImportButtonAction(ActionEvent event) {
+	private File file;
+
+	@FXML
+	protected void handleImportButtonAction(ActionEvent event) {
 		FileChooser fileChooser = new FileChooser();
 
 		fileChooser.setTitle("Select Data Descriptor File");
@@ -31,8 +47,28 @@ public class DataController {
 				new FileChooser.ExtensionFilter("XML", "*.xml")
 		);
 
-		File file = fileChooser.showOpenDialog(root.getScene().getWindow());
+		file = fileChooser.showOpenDialog(root.getScene().getWindow());
+		fileNameField.setText(file.getAbsolutePath());
+	}
 
+	@FXML
+	protected void handleAnalyseButtonAction(ActionEvent event) {
+		try {
+			XmlReader reader = new XmlReader(file);
+			DataFile dataFile = reader.getDataFiles().get(0);
+			DataReader dataReader = new DataReader();
+			DataModel input = dataReader.readData(dataFile.filterHeader());
+
+			Constraint constraint = new EqualityCheck(
+					input.getColumns().get("time"), new StringValue("0803")
+			);
+
+			DataAnalysis analysis = new ConstraintAnalysis(constraint);
+			DataModel out = analysis.analyse(input);
+			DataModelWriter.write(out, "out/dingen.txt", "\t");
+		} catch (Exception e) {
+			logger.log(Level.WARNING, "Error reading XML file", e);
+		}
 	}
 
 }
