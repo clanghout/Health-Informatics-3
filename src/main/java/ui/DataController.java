af2256f4156d048035785ebfbe38f242784989a8
Boudewijn <Boudewijn@vangroos.nl>
2015-05-06 10:21:38 +0200

    Renamed name-field to file-name-field and started opening a FileChooser

---
+++ src/main/java/ui/DataController.java
@@ -0,0 +1,35 @@
+package ui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+/**
+ * Created by Boudewijn on 6-5-2015.
+ */
+public class DataController {
+	@FXML private TextField fileNameField;
+	@FXML private Parent root;
+
+
+	@FXML protected void handleImportButtonAction(ActionEvent event) {
+		FileChooser fileChooser = new FileChooser();
+
+		fileChooser.setTitle("Select Data Descriptor File");
+		fileChooser.setInitialDirectory(
+				new File(System.getProperty("user.home"))
+		);
+		fileChooser.getExtensionFilters().add(
+				new FileChooser.ExtensionFilter("XML", "*.xml")
+		);
+
+		fileChooser.showOpenDialog(root.getScene().getWindow());
+	}
+
+}
