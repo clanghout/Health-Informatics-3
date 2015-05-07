37f33388dbabe76d2227cb7f218dca190605024b
clanghout <chris.langhout@gmail.com>
2015-05-07 23:27:54 +0200

    change DataModel.getRow to output list and fix dependancies

--- src/main/java/model/data/process/analysis/ConstraintAnalysis.java
+++ src/main/java/model/data/process/analysis/ConstraintAnalysis.java
@@ -24,9 +24,8 @@ public class ConstraintAnalysis extends DataAnalysis {
 	@Override
 	public DataModel analyse(DataModel input) {
 		List<DataRow> out = new ArrayList<>();
-		Iterator<DataRow> iterator = input.getRows();
-		while (iterator.hasNext()) {
-			DataRow row = iterator.next();
+		List<DataRow> rows = input.getRows();
+		for(DataRow row: rows){
 			if (constraint.check(row)) {
 				out.add(row);
 			}
