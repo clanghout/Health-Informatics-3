87ca6b8a134c4071aabff7c0baeacf7bfb6e1979
clanghout <chris.langhout@gmail.com>
2015-05-08 00:03:56 +0200

    improve DataModelWriter and add test file

--- src/main/java/output/DataModelWriter.java
+++ src/main/java/output/DataModelWriter.java
@@ -3,58 +3,97 @@ package output;
 import model.data.DataColumn;
 import model.data.DataModel;
 import model.data.DataRow;
-import model.reader.DataReader;
+import model.data.DataValue;
+import model.data.value.StringValue;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Logger;
 
 /**
- * Write output after analysis is done
+ * Write output after analysis is done.
  */
 public class DataModelWriter {
-	public static void write(DataModel datamodel, String filename, String delimiter){
-		Iterator rows = datamodel.getRows();
-		Map<String, DataColumn> columnsData = datamodel.getColumns();
-		List<DataColumn> columnList = new ArrayList<>();
+	private File file;
+	private DataModel dataModel;
+	private String delimiter = ",";
+	private List<DataColumn> columns;
+	private Logger logger = Logger.getLogger("DataModelWriter");
 
+	// Options for user
+	private boolean quotationmarks;
+
+	/**
+	 * Constructor for DataModelWriter.
+	 *
+	 * @param dataModelInput the output dataModel
+	 * @param fileInput      the name of the output file
+	 */
+	public DataModelWriter(DataModel dataModelInput, File fileInput) {
+		this.file = fileInput;
+		this.dataModel = dataModelInput;
+		this.columns = new ArrayList<>();
+
+		Map<String, DataColumn> columnsData = dataModel.getColumns();
 		for (Object o : columnsData.entrySet()) {
 			Map.Entry pair = (Map.Entry) o;
 			DataColumn column = (DataColumn) pair.getValue();
-			columnList.add(column);
+			columns.add(column);
 		}
+	}
 
-		try (PrintWriter writer = new PrintWriter(filename,"UTF-8")){
+	/**
+	 * Set the delimiter for the output.
+	 * Default value is ", "
+	 *
+	 * @param delimiter the delimiter to be used.
+	 */
+	public void setDelimiter(String delimiter) {
+		this.delimiter = delimiter;
+	}
 
-			while(rows.hasNext()){
-				DataRow row = (DataRow) rows.next();
-				for(DataColumn col: columnList) {
-					writer.print(row.getValue(col).toString() + delimiter);
+	/**
+	 * Creates a PrintWriter that prints the datamodel to a file.
+	 */
+	public void write() {
+		List<DataRow> rows = dataModel.getRows();
+		try (PrintWriter writer = new PrintWriter(this.file, "UTF-8")) {
+
+			for (DataRow row : rows) {
+				if (columns.size() > 0) {
+					writer.print(row.getValue(columns.get(0)).toString());
+				} else {
+					logger.warning("Zero columns specified");
+				}
+				for (int i = 1; i < columns.size(); i++) {
+					DataColumn col = columns.get(i);
+					if (quotationmarks) {
+						String value = addQuotes(row.getValue(col));
+						writer.print(delimiter + value);
+					} else {
+						writer.print(delimiter + row.getValue(col).toString());
+					}
 				}
 				writer.println();
 			}
+			logger.info("data written");
 		} catch (FileNotFoundException | UnsupportedEncodingException e) {
 			e.printStackTrace();
 		}
 	}
 
-
-
-	public static void main(String[] args) {
-		File file = new File("res/ADMIRE 2.txt");
-		DataReader dataReader = new DataReader();
-		System.out.println("datareader made");
-		try {
-			DataModel dataModel = dataReader.readData(file);
-			System.out.println("data read");
-			write(dataModel, "sample.txt", "\t");
-			System.out.println("data written");
-		} catch (IOException e) {
-			e.printStackTrace();
+	public String addQuotes(DataValue value) {
+		String val = value.toString();
+		if (value instanceof StringValue) {
+			return '"' + val + '"';
+		} else {
+			return val;
 		}
-
 	}
 }
 
