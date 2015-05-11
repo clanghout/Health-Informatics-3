d8f7189a25cd636ea9300f36dedaeebdafd9ebbd
louisgosschalk <louis@dophoeve.nl>
2015-05-11 12:05:07 +0200

    minimum
    changed output of minimum to list of datarows

--- src/main/java/model/data/process/functions/Minimum.java
+++ src/main/java/model/data/process/functions/Minimum.java
@@ -1,26 +1,37 @@
 package model.data.process.functions;
 
+import java.util.ArrayList;
 import model.data.DataModel;
-import model.data.DataRow;
 import model.data.DataValue;
+import model.data.DataRow;
 
 /**
  * A class for finding the row with the minimum value for the specified column in a model
  * @author Louis Gosschalk 
  * @date 11-05-2015
+ * what if there's a duplicate minimum?
  */
 public class Minimum {
-	public DataRow MinimumCheck(DataModel model, String columnName) {
+	
+	/**
+	 * 
+	 * @param model datamodel containing the input
+	 * @param columnName string specifying the 
+	 * @return ArrayList of DataRow, in case there's duplicate minimums
+	 */
+	public ArrayList<DataRow> MinimumCheck(DataModel model, String columnName) {
 		//return new DataRow();
+		ArrayList<DataRow> rowlist = null;
 		DataRow row = null;
 		if(model.getRowCount() != 0)
 			row = model.getRow(0);
 		else
-			return row; //throw exception "empty datamodel"
+			return rowlist; //throw exception "empty datamodel"
 		
 		//current row is the only row
 		if(model.getRowCount() == 1) {
-			return row;
+			rowlist.add(row);
+			return rowlist;
 		}
 		//loop through datamodel rows, begin at 1 because 0 is the comparison
 		for(int i=1; i< model.getRowCount(); i++){
@@ -30,9 +41,14 @@ public class Minimum {
 			DataValue compare = model.getRow(i).getValue(columnName);
 			//getvalue int van compare
 			//int compareInt = compare.getValue();
-			//if(currentInt > compareInt)
+			//if(currentInt > compareInt){
 				//row = compare;
+				//rowlist.clear();
+				//rowlist.add(compare)
+			//}
+			//else if(currentInt == compareInt)
+				//rowlist.add(compare)
 		}
-		return row;
+		return rowlist;
 	}
 }
