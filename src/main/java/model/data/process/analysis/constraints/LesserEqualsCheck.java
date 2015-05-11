615741d3481a29bb56c86979b043a629300a7698
Boudewijn <boudewijn@vangroos.nl>
2015-05-11 19:03:51 +0200

    Added the comparison checks.

---
+++ src/main/java/model/data/process/analysis/constraints/LesserEqualsCheck.java
@@ -0,0 +1,23 @@
+package model.data.process.analysis.constraints;
+
+import model.data.DataRow;
+import model.data.describer.DataDescriber;
+import model.data.value.DataValue;
+
+/**
+ * The check if a value is lesser than or equal to another.
+ * Created by Boudewijn on 11-5-2015.
+ * @param <T> The type of the values being compared.
+ */
+public final class LesserEqualsCheck<T extends DataValue & Comparable<DataValue>>
+		extends CompareCheck<T> {
+
+	public LesserEqualsCheck(DataDescriber<T> leftSide, DataDescriber<T> rightSide) {
+		super(leftSide, rightSide);
+	}
+
+	@Override
+	public boolean check(DataRow row) {
+		return compare(row) <= 0;
+	}
+}
