ca545254d8bf757ab5549484a39831523e021b04
louisgosschalk <louis@dophoeve.nl>
2015-05-12 10:11:02 +0200

    Minimum & maximum werkend
    Deze versie zou een functionerende implementatie van minimum en maximum
    moeten zijn

--- src/main/java/model/data/process/functions/Maximum.java
+++ src/main/java/model/data/process/functions/Maximum.java
@@ -1,8 +1,12 @@
 package model.data.process.functions;
 
 import java.util.ArrayList;
+import java.util.List;
+
 import model.data.DataModel;
 import model.data.DataRow;
+import model.data.value.FloatValue;
+import model.data.value.IntValue;
 
 /**
  * A class for finding the row with the minimum value for the specified column in a model
@@ -13,63 +17,58 @@ public class Maximum {
 	
 	private DataModel model;
 	private String name;
-	private ArrayList<DataRow> rowlist;
+	private List<DataRow> rowlist;
 	private DataRow row;
 	
 	public Maximum(DataModel model, String columnName) {
 		this.model = model;
 		this.name = columnName;
-		this.rowlist = null;
+		this.rowlist = new ArrayList<>();
 		this.row = null;
 	}
 	/**
 	 * @param model datamodel containing the input
 	 * @param columnName string specifying the 
-	 * @return ArrayList of DataRow; because duplicate minimums are possible.
+	 * @return List of DataRow; because duplicate minimums are possible.
 	 */
-	public ArrayList<DataRow> MaximumCheck() {
-		//return new DataRow();
-//		ArrayList<DataRow> rowlist = null;
-//		DataRow row = null;
+	public List<DataRow> maximumCheck() {
 		if(model.getRowCount() != 0)
 			row = model.getRow(0);
 		else
 			return rowlist; 
-			/**
-			 * return null or throw exception if input is null?
-			 */
-		
-		//current row is the only row
+
 		if(model.getRowCount() == 1) {
 			rowlist.add(row);
 			return rowlist;
 		}
-		//loop through datamodel rows, begin at 1 because 0 is the comparison
-		for(int i=1; i< model.getRowCount(); i++){
-			/**
-			 * check if specified column is int or float, else throw exception
-			 */ 
-			String klasse = CheckClass(row.getValue(name).getValue());
-			Compare(i);
-			else if(klass == float) FloatCompare(i)
-			
-//			if(row.getValue(name).getValue().getClass().isInstance(int.class))
-		}
+		/**
+		 * Check type of specified column
+		 */
+		if(model.getColumns().get(name).getType().equals(FloatValue.class))
+			 compare();
+		else if(model.getColumns().get(name).getType().equals(IntValue.class))
+			 compare();
+		else rowlist.clear();
+		
 		return rowlist;
 	}
-	public ArrayList<DataRow> Compare(int i) {
-		int currentInt = (int) row.getValue(name).getValue();
-		DataRow compare = model.getRow(i);
-		int compareInt = (int) compare.getValue(name).getValue();
-		if(currentInt > compareInt){
-			row = compare;
-			rowlist.clear();
-			rowlist.add(compare);
-			return rowlist;
-		}
-		else if(currentInt == compareInt){
-			rowlist.add(compare);
-			return rowlist;
+	/**
+	 * Get values and cast to Float, determine maximum values
+	 * @return
+	 */
+	public List<DataRow> compare() {
+		for(int i = 0; i<model.getRowCount(); i++){
+			float currentVal = (float) row.getValue(name).getValue();
+			DataRow compare = model.getRow(i);
+			float compareVal = (float) compare.getValue(name).getValue();
+			if(currentVal > compareVal){
+				row = compare;
+				rowlist.clear();
+				rowlist.add(compare);
+			}
+			else if(currentVal == compareVal){
+				rowlist.add(compare);
+			}
 		}
 		return rowlist;
 	}
--- src/main/java/model/data/process/functions/Minimum.java
+++ src/main/java/model/data/process/functions/Minimum.java
@@ -1,9 +1,12 @@
 package model.data.process.functions;
 
 import java.util.ArrayList;
+import java.util.List;
+
 import model.data.DataModel;
-import model.data.DataValue;
 import model.data.DataRow;
+import model.data.value.FloatValue;
+import model.data.value.IntValue;
 
 /**
  * A class for finding the row with the minimum value for the specified column in a model
@@ -12,44 +15,60 @@ import model.data.DataRow;
  */
 public class Minimum {
 	
+	private DataModel model;
+	private String name;
+	private List<DataRow> rowlist;
+	private DataRow row;
+	
+	public Minimum(DataModel model, String columnName) {
+		this.model = model;
+		this.name = columnName;
+		this.rowlist = new ArrayList<>();
+		this.row = null;
+	}
 	/**
-	 * 
 	 * @param model datamodel containing the input
 	 * @param columnName string specifying the 
-	 * @return ArrayList of DataRow, in case there's duplicate minimums
+	 * @return List of DataRow; because duplicate minimums are possible.
 	 */
-	public ArrayList<DataRow> MinimumCheck(DataModel model, String columnName) {
-		//return new DataRow();
-		ArrayList<DataRow> rowlist = null;
-		DataRow row = null;
+	public List<DataRow> minimumCheck() {
 		if(model.getRowCount() != 0)
 			row = model.getRow(0);
 		else
 			return rowlist; 
-			/**
-			 * return null or throw exception if input is null?
-			 */
-		
-		//current row is the only row
+
 		if(model.getRowCount() == 1) {
 			rowlist.add(row);
 			return rowlist;
 		}
-		//loop through datamodel rows, begin at 1 because 0 is the comparison
-		for(int i=1; i< model.getRowCount(); i++){
-			/**
-			 * check if specified column is int, else throw exception
-			 */
-			int currentInt = (int) row.getValue(columnName).getValue();
+		/**
+		 * Check type of specified column
+		 */
+		if(model.getColumns().get(name).getType().equals(FloatValue.class))
+			 compare();
+		else if(model.getColumns().get(name).getType().equals(IntValue.class))
+			 compare();
+		else rowlist.clear();
+		
+		return rowlist;
+	}
+	/**
+	 * Get values and cast to Float, determine minimum values
+	 * @return
+	 */
+	public List<DataRow> compare() {
+		for(int i = 0; i<model.getRowCount(); i++){
+			float currentVal = (float) row.getValue(name).getValue();
 			DataRow compare = model.getRow(i);
-			int compareInt = (int) compare.getValue(columnName).getValue();
-			if(currentInt > compareInt){
+			float compareVal = (float) compare.getValue(name).getValue();
+			if(currentVal < compareVal){
 				row = compare;
 				rowlist.clear();
 				rowlist.add(compare);
 			}
-			else if(currentInt == compareInt)
+			else if(currentVal == compareVal){
 				rowlist.add(compare);
+			}
 		}
 		return rowlist;
 	}
