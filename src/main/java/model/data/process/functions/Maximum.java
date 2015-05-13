9a2c961b5641c8bc09cc5572aa7ea3ad7883162e
louisgosschalk <louis@dophoeve.nl>
2015-05-13 12:43:56 +0200

    Onnodigheid uit maximum en minimum verwijderd

--- src/main/java/model/data/process/functions/Maximum.java
+++ src/main/java/model/data/process/functions/Maximum.java
@@ -1,9 +1,5 @@
 package model.data.process.functions;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import model.data.DataRow;
 import model.data.DataTable;
 import model.data.describer.DataDescriber;
 import model.data.value.NumberValue;
@@ -15,17 +11,8 @@ import model.data.value.NumberValue;
  */
 public class Maximum extends Function{
 	
-	private DataTable model;
-	private DataDescriber<NumberValue> argument;
-	private ArrayList<DataRow> rowlist;
-	private DataRow row;
-	
 	public Maximum(DataTable model, DataDescriber<NumberValue> argument) {
 		super(model, argument);
-		this.model = model;
-		this.argument = argument;
-		this.row = model.getRow(0);
-		this.rowlist = new ArrayList<DataRow>();
 		maximum = true;
 	}
 }
