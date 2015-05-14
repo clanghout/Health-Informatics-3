66aa54cbccd1b3faaecbe57cebe475595aaa77d7
Paul <P.J.vanWijk@student.tudelft.nl>
2015-05-14 21:33:21 +0000

    add extra tests for datafile

--- src/test/java/xml/filetype/DataFileTest.java
+++ src/test/java/xml/filetype/DataFileTest.java
@@ -13,7 +13,7 @@ import xml.filetype.PlainTextFile;
 public class DataFileTest {
 
 	@Test
-	public void testCreateDataFile() throws Exception {
+	public void testCreateDataFile() {
 		String file = getClass().getResource("/input/statsensor.txt").getFile();
 
 		DataFile df = DataFile.createDataFile(file, "plaintext");
@@ -21,14 +21,32 @@ public class DataFileTest {
 		
 		df.setStartLine(7);
 		df.setEndLine(8);
-		InputStream st = df.getDataStream();
+		InputStream st;
+		try {
+			st = df.getDataStream();
 		BufferedReader reader = new BufferedReader(new InputStreamReader(st));
 		assertEquals("Crea,	179,umol/L,00,130218,0802", reader.readLine());
 		assertEquals("Crea,	179,umol/L,00,130218,0803", reader.readLine());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 	
 	@Test(expected = DataFileNotRecognizedException.class)
 	public void testCreateInvalidDataFile() {
 		DataFile.createDataFile("/input/statsensor.txt", "invalidType");
+	}
+	
+	@Test
+	public void testCreateXlsFile(){
+		DataFile df = DataFile.createDataFile("/input/xlsfile.xls", "xls");
+		assertTrue(df instanceof XlsFile);
+	}
+
+	@Test
+	public void testCreateXlsxFile(){
+		DataFile df = DataFile.createDataFile("/input/xlsxfile.xlsx", "xlsx");
+		assertTrue(df instanceof XlsxFile);
 	}
 }
