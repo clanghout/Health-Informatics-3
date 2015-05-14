89d5a3d265c9e201e652ec510a6e96403bc3c690
Paul <P.J.vanWijk@student.tudelft.nl>
2015-05-14 21:22:42 +0000

    implement xlsFile class and add test xls file

--- src/main/java/xml/filetype/XlsFile.java
+++ src/main/java/xml/filetype/XlsFile.java
@@ -1,18 +1,52 @@
 package xml.filetype;
 
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Iterator;
+
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 
 public class XlsFile extends DataFile {
 
 	public XlsFile(String path) {
 		super(path);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public InputStream getDataStream() throws FileNotFoundException {
-		// TODO Auto-generated method stub
+		
+		FileInputStream file = new FileInputStream(getFile());
+		try {
+			HSSFWorkbook workbook = new HSSFWorkbook(file);
+			HSSFSheet sheet = workbook.getSheetAt(0);
+			Iterator<Row> rowIterator = sheet.iterator();
+			String res = "";
+			while(rowIterator.hasNext()) {
+				Row row = rowIterator.next();
+				Iterator<Cell> cellIterator = row.iterator();
+				
+				while(cellIterator.hasNext()) {
+					Cell cell = cellIterator.next();
+                    res += cell.getStringCellValue() + "\t";
+				}
+				res += "\n";
+			}
+			InputStream newStream = new ByteArrayInputStream(
+			        	res.getBytes(StandardCharsets.UTF_8)
+					);
+			return newStream;
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 		return null;
 	}
 }
--- src/test/java/xml/filetype/XlsFileTest.java
+++ src/test/java/xml/filetype/XlsFileTest.java
@@ -2,13 +2,36 @@ package xml.filetype;
 
 import static org.junit.Assert.*;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class XlsFileTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	XlsFile xlsFile;
+	
+	@Before
+	public void setUp() {
+		String file = getClass().getResource("/input/xlsfile.xls").getFile();
+		xlsFile = new XlsFile(file);
 	}
-
+	
+	@Test
+	public void testDataStream() {
+		try {
+			InputStream stream = xlsFile.getDataStream();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+			assertEquals("foo\tbar\t", reader.readLine());
+			assertEquals("row2\tbar\tbar\tbat\t", reader.readLine());
+			assertEquals("row3\tbar\tbar\tbats\t", reader.readLine());
+			assertNull(reader.readLine());
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
