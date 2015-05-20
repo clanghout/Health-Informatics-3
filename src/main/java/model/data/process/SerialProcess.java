346b04a73e45474a52c3a99f43292a98cd611b97
Boudewijn <boudewijn@vangroos.nl>
2015-05-20 20:02:32 +0200

    Created the basis for the SerialProcess.

---
+++ src/main/java/model/data/process/SerialProcess.java
@@ -0,0 +1,33 @@
+package model.data.process;
+
+import model.data.DataTable;
+
+import java.util.List;
+
+/**
+ * Represents a DataProcess chain, where the output of the first process is piped into the next.
+ *
+ * Created by Boudewijn on 20-5-2015.
+ */
+public class SerialProcess extends DataProcess {
+
+	private List<DataProcess> processList;
+
+	/**
+	 * Construct a new SerialProcess.
+	 * @param processList The process chain for this process.
+	 */
+	public SerialProcess(List<DataProcess> processList) {
+		this.processList = processList;
+	}
+
+	@Override
+	protected DataTable doProcess() {
+		DataTable result = getInput();
+		for (DataProcess i : processList) {
+			i.setInput(result);
+			result = i.process();
+		}
+		return result;
+	}
+}
---
+++ src/test/java/model/data/process/SerialProcessTest.java
@@ -0,0 +1,26 @@
+package model.data.process;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Contains the tests for the SerialProcess.
+ *
+ * Created by Boudewijn on 20-5-2015.
+ */
+public class SerialProcessTest {
+
+	private DataProcess returnNothing;
+
+	@Before
+	public void setUp() throws Exception {
+
+
+	}
+
+	@Test
+	public void testProcess() throws Exception {
+
+
+	}
+}
