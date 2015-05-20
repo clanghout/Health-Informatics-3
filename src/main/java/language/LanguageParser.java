3189e3b0c398d8bab6ec5e8a1df2dc9df5d23e31
Boudewijn <Boudewijn@vangroos.nl>
2015-05-20 10:42:17 +0200

    Created the first parser class and added parboiled to the pom

---
+++ src/main/java/language/LanguageParser.java
@@ -0,0 +1,12 @@
+package language;
+
+import model.data.process.DataProcess;
+import org.parboiled.BaseParser;
+import org.parboiled.Rule;
+
+/**
+ * Created by Boudewijn on 20-5-2015.
+ */
+public class LanguageParser extends BaseParser<DataProcess> {
+
+}
