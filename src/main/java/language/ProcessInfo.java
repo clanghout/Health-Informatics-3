f03eb81ec38a47e953a225777f351c62a038b101
Boudewijn <boudewijn@vangroos.nl>
2015-05-20 17:25:26 +0200

    Implemented the inner workings of ProcessInfo.

--- src/main/java/language/ProcessInfo.java
+++ src/main/java/language/ProcessInfo.java
@@ -1,11 +1,25 @@
 package language;
 
 /**
+ * Contains the information required to produce a DataProcess.
+ *
  * Created by Boudewijn on 20-5-2015.
  */
 public class ProcessInfo {
 
-	public ProcessInfo(Identifier<Object> name, Object[] parameters) {
+	private Identifier<Object> name;
+	private Object[] parameters;
 
+	public ProcessInfo(Identifier<Object> name, Object[] parameters) {
+		this.name = name;
+		this.parameters = parameters;
+	}
+
+	public Object[] getParameters() {
+		return parameters;
+	}
+
+	public Identifier<Object> getIdentifier() {
+		return name;
 	}
 }
