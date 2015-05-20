9c857a4f7184507f608148d1a0ed715f425c21fd
Boudewijn <Boudewijn@vangroos.nl>
2015-05-20 12:03:24 +0200

    Added the first rules for our Parser and created some tests for them.
    I'm in love with parboiled.

--- src/main/java/language/LanguageParser.java
+++ src/main/java/language/LanguageParser.java
@@ -5,8 +5,102 @@ import org.parboiled.BaseParser;
 import org.parboiled.Rule;
 
 /**
+ * The parser for the analysis language.
+ *
+ * Since this class is rather declarative in its working, the style deviates from the default Java style and is more
+ * PEG based.
+ *
  * Created by Boudewijn on 20-5-2015.
  */
+@SuppressWarnings("ALL")
 public class LanguageParser extends BaseParser<DataProcess> {
 
+	Rule Identifier() {
+		return Sequence(
+				FirstOf(
+						CharRange('a', 'z'),
+						CharRange('A', 'Z')
+				), OneOrMore(
+						FirstOf(
+								Digit(),
+								CharRange('a', 'z'),
+								CharRange('A', 'Z')
+						)
+				)
+		);
+	}
+
+	Rule StringLiteral() {
+		return Sequence(
+				"\"",
+				ZeroOrMore(Character()),
+				"\"");
+	}
+
+	Rule Character() {
+		return NormalCharacter();
+	}
+
+	Rule NormalCharacter() {
+		return Sequence(TestNot(AnyOf("\"")), ANY);
+	}
+
+	Rule IntLiteral() {
+		return OneOrMore(Digit());
+	}
+
+	Rule FloatLiteral() {
+		return Sequence(
+				OneOrMore(Digit()),
+				".",
+				OneOrMore(Digit())
+		);
+	}
+
+	Rule Variable() {
+		return FirstOf(Identifier(), StringLiteral(), FloatLiteral(), IntLiteral());
+	}
+
+	Rule Params() {
+		return Sequence(
+				"(",
+				Optional(ParamDecl()),
+				")"
+		);
+	}
+
+	Rule ParamDecl() {
+		return Sequence(Variable(), ParamRest());
+	}
+
+	Rule ParamRest() {
+		return Optional(Sequence(",", WhiteSpace(), ParamDecl()));
+	}
+
+	Rule WhiteSpace() {
+		return ZeroOrMore(WhiteSpaceChars());
+	}
+
+	Rule WhiteSpaceChars() {
+		return FirstOf(" ", "\t");
+	}
+
+	Rule Process() {
+		return Sequence(Identifier(), Params());
+	}
+
+	Rule Pipe() {
+		return Sequence(
+				Process(),
+				"|",
+				FirstOf(
+						Pipe(),
+						Process()
+				)
+		);
+	}
+
+	Rule Digit() {
+		return CharRange('0', '9');
+	}
 }
