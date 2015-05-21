47aa7fa94219eb36eb0970c3fa557a6c2590c3a3
Boudewijn <boudewijn@vangroos.nl>
2015-05-22 00:34:21 +0200

    Slightly improved MacroType

--- src/main/java/language/MacroType.java
+++ src/main/java/language/MacroType.java
@@ -2,11 +2,12 @@ package language;
 
 import model.data.DataModel;
 import model.data.process.analysis.operations.Operation;
-import org.parboiled.BaseParser;
 import org.parboiled.Parboiled;
 import org.parboiled.parserunners.BasicParseRunner;
 
 /**
+ * Represents the type of a Macro.
+ *
  * Created by Boudewijn on 21-5-2015.
  */
 class MacroType {
@@ -23,6 +24,8 @@ class MacroType {
 			BasicParseRunner runner = new BasicParseRunner(parser.Comparison());
 			CompareNode node = (CompareNode) runner.getValueStack().pop();
 			return node.resolve(model);
-		} else throw new UnsupportedOperationException("Code has not yet been implemented");
+		} else {
+			throw new UnsupportedOperationException("Code has not yet been implemented");
+		}
 	}
 }
