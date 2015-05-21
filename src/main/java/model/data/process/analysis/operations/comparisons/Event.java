698f374e99a989df7c5ec85262971f3f7b089709
louisgosschalk <louis@dophoeve.nl>
2015-05-21 13:36:40 +0200

    opbouw comparison idee

---
+++ src/main/java/model/data/process/analysis/operations/comparisons/Event.java
@@ -0,0 +1,10 @@
+package model.data.process.analysis.operations.comparisons;
+
+/**
+ * Here I intend to create a specification for events.
+ * 
+ * @author Louis Gosschalk 21-05-2015
+ */
+public class Event {
+
+}
