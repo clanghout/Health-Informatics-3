ad4d40cada622a0c7274443c5e39e5e97e88a59a
louisgosschalk <louis@dophoeve.nl>
2015-05-22 10:57:45 +0200

    Event higher level
    Events are going to be used in multiple C’s, so it’s not the same level
    as comparison

---
+++ src/main/java/model/data/process/analysis/operations/Event.java
@@ -0,0 +1,11 @@
+package model.data.process.analysis.operations;
+
+/**
+ * Here I intend to create a specification for events.
+ * TODO: deze klasse gaat X niveau(s) hoger omdat een event algemeen gebruikt kan worden.
+ * TODO: deze klasse beschrijft heel algemeen (mbv contraints) een event
+ * @author Louis Gosschalk 21-05-2015
+ */
+public class Event {
+
+}
