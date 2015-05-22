07fde7464383d54be7e240399e8246a852126383
louisgosschalk <louis@dophoeve.nl>
2015-05-22 11:35:51 +0200

    javadoc

--- src/main/java/model/data/process/analysis/operations/Event.java
+++ src/main/java/model/data/process/analysis/operations/Event.java
@@ -2,7 +2,6 @@ package model.data.process.analysis.operations;
 
 /**
  * Here I intend to create a specification for events.
- * TODO: deze klasse gaat X niveau(s) hoger omdat een event algemeen gebruikt kan worden.
  * TODO: deze klasse beschrijft heel algemeen (mbv contraints) een event
  * @author Louis Gosschalk 21-05-2015
  */
