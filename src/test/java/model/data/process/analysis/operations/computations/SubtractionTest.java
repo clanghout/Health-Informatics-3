d26f4fd65198731cb6cb65bd40a858ffe6949468
clanghout <chris.langhout@gmail.com>
2015-05-26 09:08:25 +0200

    fix spelling errors

--- src/test/java/model/data/process/analysis/operations/computations/SubtractionTest.java
+++ src/test/java/model/data/process/analysis/operations/computations/SubtractionTest.java
@@ -8,19 +8,19 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 /**
- * Tests for the substraction computation.
+ * Tests for the subtraction computation.
  * Created by Chris on 12-5-2015.
  */
 public class SubtractionTest {
 
 	@Test
-	public void testSubstractionIntConstantsZero() throws Exception {
+	public void testSubtractionIntConstantsZero() throws Exception {
 		Subtraction check = new Subtraction<>(new ConstantDescriber<>(new IntValue(0)), new ConstantDescriber<>(new IntValue(0)));
 		assertEquals(new IntValue(0), check.compute(null));
 	}
 
 	@Test
-	public void testSubstractionIntConstants() throws Exception {
+	public void testSubtractionIntConstants() throws Exception {
 		Subtraction check = new Subtraction<>(new ConstantDescriber<>(new IntValue(456)), new ConstantDescriber<>(new IntValue(123)));
 		assertEquals(new IntValue(333), check.compute(null));
 		check = new Subtraction<>(new ConstantDescriber<>(new IntValue(123)), new ConstantDescriber<>(new IntValue(456)));
@@ -28,13 +28,13 @@ public class SubtractionTest {
 	}
 
 	@Test
-	public void testSubstractionFloatConstantsZero() throws Exception {
+	public void testSubtractionFloatConstantsZero() throws Exception {
 		Subtraction check = new Subtraction<>(new ConstantDescriber<>(new FloatValue(0f)), new ConstantDescriber<>(new FloatValue(0f)));
 		assertEquals(new FloatValue(0f), check.compute(null));
 	}
 
 	@Test
-	public void testSubstractionFloatConstants() throws Exception {
+	public void testSubtractionFloatConstants() throws Exception {
 		Subtraction check = new Subtraction<>(new ConstantDescriber<>(new FloatValue(1)), new ConstantDescriber<>(new FloatValue(0.125f)));
 		assertEquals(new FloatValue(0.875f), check.compute(null));
 	}
