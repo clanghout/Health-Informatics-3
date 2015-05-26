8bbbb5892f3cd47828cb61b581042e3aff60fa9e
jenslangerak <J.Langerak@student.tudelft.nl>
2015-05-26 10:49:17 +0200

    add before to combeRow test

--- src/test/java/model/data/CombinedDataRowTest.java
+++ src/test/java/model/data/CombinedDataRowTest.java
@@ -1,6 +1,8 @@
 package model.data;
 
+import model.data.value.DataValue;
 import model.data.value.StringValue;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,50 +12,68 @@ import static org.junit.Assert.*;
  */
 public class CombinedDataRowTest {
 
+
+	private DataColumn column1;
+	private DataColumn column2;
+	private DataRow row1;
+	private DataRow row2;
+
+	@Before
+	public void setUp() throws Exception {
+		column1 = new DataColumn("test1", new DataTable(), StringValue.class);
+		column2 = new DataColumn("test2", new DataTable(), StringValue.class);
+
+		row1 = new DataRow();
+		row1.setValue(column1, new StringValue("test"));
+
+		row2 = new DataRow();
+		row2.setValue(column2, new StringValue("test2"));
+
+	}
+
 	@Test
 	public void testAddDataRow() throws Exception {
 		CombinedDataRow combRow = new CombinedDataRow();
-		DataColumn column1 = new DataColumn("test1", null, StringValue.class);
-		DataRow dataRow = new DataRow();
-		dataRow.setValue(column1, new StringValue("test"));
 
-		combRow.addDataRow(dataRow);
+		combRow.addDataRow(row1);
 		assertEquals(combRow.getValue(column1).toString(), "test");
 	}
 
 	@Test
 	public void testGetValue() throws Exception {
 		CombinedDataRow combRow = new CombinedDataRow();
-		DataColumn column1 = new DataColumn("test1", new DataTable(), StringValue.class);
-		DataRow dataRow = new DataRow();
-		dataRow.setValue(column1, new StringValue("test"));
-
-		DataColumn column2 = new DataColumn("test1", new DataTable(), StringValue.class);
-		DataRow dataRow2 = new DataRow();
-		dataRow.setValue(column2, new StringValue("test2"));
-
-		combRow.addDataRow(dataRow);
-		combRow.addDataRow(dataRow2);
+		combRow.addDataRow(row1);
+		combRow.addDataRow(row2);
 		assertEquals(combRow.getValue(column1).toString(), "test");
 		assertEquals(combRow.getValue(column2).toString(), "test2");
+	}
+
+	@Test
+	public void testGetValueSameColumnName() throws Exception {
+		CombinedDataRow combRow = new CombinedDataRow();
+		combRow.addDataRow(row1);
+		combRow.addDataRow(row2);
+
+		DataColumn column3 = new DataColumn("test1", new DataTable(), StringValue.class);
+		DataRow row3 = new DataRow();
+		row3.setValue(column3, new StringValue("test3"));
+
+		combRow.addDataRow(row3);
+		assertEquals(combRow.getValue(column1).toString(), "test");
+		assertEquals(combRow.getValue(column2).toString(), "test2");
+		assertEquals(combRow.getValue(column3).toString(), "test3");
+
 	}
 
 	@Test
 	public void testhasColumnTrue() throws Exception {
 		CombinedDataRow combRow = new CombinedDataRow();
 
-		DataColumn column1 = new DataColumn("test1", null, StringValue.class);
-		DataRow dataRow = new DataRow();
-		dataRow.setValue(column1, new StringValue("test"));
-		assertTrue(dataRow.hasColumn(column1));
 
-		DataColumn column2 = new DataColumn("test2", null, StringValue.class);
-		DataRow dataRow2 = new DataRow();
-		dataRow2.setValue(column2, new StringValue("test"));
-		assertTrue(dataRow2.hasColumn(column2));
+		assertTrue(row2.hasColumn(column2));
 
-		combRow.addDataRow(dataRow);
-		combRow.addDataRow(dataRow2);
+		combRow.addDataRow(row1);
+		combRow.addDataRow(row2);
 		assertTrue(combRow.hasColumn(column1));
 		assertTrue(combRow.hasColumn(column2));
 	}
@@ -61,15 +81,7 @@ public class CombinedDataRowTest {
 	@Test
 	public void testhasColumnFalse() throws Exception {
 		CombinedDataRow combRow = new CombinedDataRow();
-		DataColumn column1 = new DataColumn("test1", new DataTable(), StringValue.class);
-		DataRow dataRow = new DataRow();
-		dataRow.setValue(column1, new StringValue("test"));
-
-		DataColumn column2 = new DataColumn("test1", new DataTable(), StringValue.class);
-		DataRow dataRow2 = new DataRow();
-		dataRow2.setValue(column2, new StringValue("test2"));
-
-		combRow.addDataRow(dataRow);
+		combRow.addDataRow(row1);
 
 		assertFalse(combRow.hasColumn(column2));
 	}
@@ -78,16 +90,9 @@ public class CombinedDataRowTest {
 	@Test
 	public void testSetValue() throws Exception {
 		CombinedDataRow combRow = new CombinedDataRow();
-		DataColumn column1 = new DataColumn("test1", new DataTable(), StringValue.class);
-		DataRow dataRow = new DataRow();
-		dataRow.setValue(column1, new StringValue("test"));
 
-		DataColumn column2 = new DataColumn("test1", new DataTable(), StringValue.class);
-		DataRow dataRow2 = new DataRow();
-		dataRow.setValue(column2, new StringValue("test2"));
-
-		combRow.addDataRow(dataRow);
-		combRow.addDataRow(dataRow2);
+		combRow.addDataRow(row1);
+		combRow.addDataRow(row2);
 		assertEquals(combRow.getValue(column1).toString(), "test");
 		assertEquals(combRow.getValue(column2).toString(), "test2");
 
@@ -95,13 +100,38 @@ public class CombinedDataRowTest {
 		assertEquals(combRow.getValue(column1).toString(), "test3");
 	}
 
+	@Test
+	public void testSetValueSameColumnName() throws Exception {
+		CombinedDataRow combRow = new CombinedDataRow();
+		combRow.addDataRow(row1);
+		combRow.addDataRow(row2);
+
+		DataColumn column3 = new DataColumn("test1", new DataTable(), StringValue.class);
+		DataRow row3 = new DataRow();
+		row3.setValue(column3, new StringValue("test3"));
+
+		combRow.addDataRow(row3);
+		assertEquals(combRow.getValue(column1).toString(), "test");
+		assertEquals(combRow.getValue(column2).toString(), "test2");
+		assertEquals(combRow.getValue(column3).toString(), "test3");
+
+		combRow.setValue(column1, new StringValue("test5"));
+		assertEquals(combRow.getValue(column1).toString(), "test5");
+		assertEquals(combRow.getValue(column2).toString(), "test2");
+		assertEquals(combRow.getValue(column3).toString(), "test3");
+
+		combRow.setValue(column3, new StringValue("test6"));
+		assertEquals(combRow.getValue(column1).toString(), "test5");
+		assertEquals(combRow.getValue(column2).toString(), "test2");
+		assertEquals(combRow.getValue(column3).toString(), "test6");
+
+	}
+
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testGetValueException() throws Exception {
 		CombinedDataRow combRow = new CombinedDataRow();
-		DataColumn column1 = new DataColumn("test1", null, StringValue.class);
-		DataRow dataRow = new DataRow();
-		dataRow.setValue(column1, new StringValue("test"));
-		combRow.addDataRow(dataRow);
+		combRow.addDataRow(row1);
 
 		combRow.getValue(new DataColumn("test2", null, StringValue.class));
 	}
@@ -109,10 +139,7 @@ public class CombinedDataRowTest {
 	@Test(expected = IllegalArgumentException.class)
 	public void testSetValueException() throws Exception {
 		CombinedDataRow combRow = new CombinedDataRow();
-		DataColumn column1 = new DataColumn("test1", null, StringValue.class);
-		DataRow dataRow = new DataRow();
-		dataRow.setValue(column1, new StringValue("test"));
-		combRow.addDataRow(dataRow);
+		combRow.addDataRow(row1);
 
 		combRow.setValue(new DataColumn("test2", null, StringValue.class), new StringValue("test2"));
 	}
@@ -120,35 +147,21 @@ public class CombinedDataRowTest {
 	@Test
 	public void testGetRows() throws Exception {
 		CombinedDataRow combRow = new CombinedDataRow();
-		DataColumn column1 = new DataColumn("test1", null, StringValue.class);
-		DataRow dataRow = new DataRow();
-		dataRow.setValue(column1, new StringValue("test"));
 
-		DataColumn column2 = new DataColumn("test1", null, StringValue.class);
-		DataRow dataRow2 = new DataRow();
-		dataRow.setValue(column2, new StringValue("test2"));
-
-		combRow.addDataRow(dataRow);
-		combRow.addDataRow(dataRow2);
+		combRow.addDataRow(row1);
+		combRow.addDataRow(row2);
 
 		assertEquals(combRow.getRows().size(), 2);
-		assertTrue(combRow.getRows().contains(dataRow));
-		assertTrue(combRow.getRows().contains(dataRow2));
+		assertTrue(combRow.getRows().contains(row1));
+		assertTrue(combRow.getRows().contains(row2));
 	}
 
 	@Test
 	public void testCopy() throws Exception {
 		CombinedDataRow combRow = new CombinedDataRow();
-		DataColumn column1 = new DataColumn("test1", null, StringValue.class);
-		DataRow dataRow = new DataRow();
-		dataRow.setValue(column1, new StringValue("test"));
 
-		DataColumn column2 = new DataColumn("test1", null, StringValue.class);
-		DataRow dataRow2 = new DataRow();
-		dataRow.setValue(column2, new StringValue("test2"));
-
-		combRow.addDataRow(dataRow);
-		combRow.addDataRow(dataRow2);
+		combRow.addDataRow(row1);
+		combRow.addDataRow(row2);
 
 		assertEquals(combRow, combRow.copy());
 	}
@@ -156,19 +169,12 @@ public class CombinedDataRowTest {
 	@Test
 	public void testEqualsTrue() throws Exception {
 		CombinedDataRow combRow = new CombinedDataRow();
-		DataColumn column1 = new DataColumn("test1", null, StringValue.class);
-		DataRow dataRow = new DataRow();
-		dataRow.setValue(column1, new StringValue("test"));
 
-		DataColumn column2 = new DataColumn("test1", null, StringValue.class);
-		DataRow dataRow2 = new DataRow();
-		dataRow.setValue(column2, new StringValue("test2"));
-
-		combRow.addDataRow(dataRow);
-		combRow.addDataRow(dataRow2);
+		combRow.addDataRow(row1);
+		combRow.addDataRow(row2);
 		CombinedDataRow copy = new CombinedDataRow();
-		copy.addDataRow(dataRow);
-		copy.addDataRow(dataRow2);
+		copy.addDataRow(row1);
+		copy.addDataRow(row2);
 
 		assertTrue(combRow.equals(copy));
 	}
@@ -176,18 +182,11 @@ public class CombinedDataRowTest {
 	@Test
 	public void testEqualsFalse() throws Exception {
 		CombinedDataRow combRow = new CombinedDataRow();
-		DataColumn column1 = new DataColumn("test1", null, StringValue.class);
-		DataRow dataRow = new DataRow();
-		dataRow.setValue(column1, new StringValue("test"));
 
-		DataColumn column2 = new DataColumn("test1", null, StringValue.class);
-		DataRow dataRow2 = new DataRow();
-		dataRow.setValue(column2, new StringValue("test2"));
-
-		combRow.addDataRow(dataRow);
-		combRow.addDataRow(dataRow2);
+		combRow.addDataRow(row1);
+		combRow.addDataRow(row2);
 		CombinedDataRow copy = new CombinedDataRow();
-		copy.addDataRow(dataRow);
+		copy.addDataRow(row1);
 
 		assertFalse(combRow.equals(copy));
 	}
