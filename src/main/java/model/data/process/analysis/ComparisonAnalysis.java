4e6995a1d6ca482152e7cbd03724469d385866cf
louisgosschalk <louis@dophoeve.nl>
2015-05-26 12:21:27 +0200

    corrigeren WIP

--- src/main/java/model/data/process/analysis/ComparisonAnalysis.java
+++ src/main/java/model/data/process/analysis/ComparisonAnalysis.java
@@ -4,16 +4,13 @@ import model.data.Table;
 import model.data.process.analysis.operations.comparisons.Comparison;
 
 /**
- * Implements comparison analysis
- * 
+ * Implements comparison analysis.
+ * Not yet though.
  * @author Louis Gosschalk 26-05-2015
  */
 public class ComparisonAnalysis extends DataAnalysis {
 	
-	private final Comparison comparison;
-	
 	public ComparisonAnalysis(Comparison comparison) {
-		this.comparison = comparison;
 	}
 
 	@Override
