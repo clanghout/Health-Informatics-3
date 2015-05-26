b97ff1e066a4de95584f347efa5be80af944b999
louisgosschalk <louis@dophoeve.nl>
2015-05-26 11:41:59 +0200

    Opzetje laganalysis

--- src/main/java/model/data/process/analysis/operations/comparisons/LagAnalysis.java
+++ src/main/java/model/data/process/analysis/operations/comparisons/LagAnalysis.java
@@ -1,10 +1,31 @@
 package model.data.process.analysis.operations.comparisons;
 
+import model.data.Table;
+import model.data.process.analysis.operations.constraints.Constraint;
+
 /**
  * This class will calculate the lag between specified events.
  * 
- * @author Louis Gosschalk 21-05-2015
+ * @author Louis Gosschalk 26-05-2015
  */
-public class LagAnalysis {
+public class LagAnalysis extends Comparison {
+
+	private Table table;
+
+	public LagAnalysis(Table table, Constraint constraint) {
+		super(table, constraint);
+		this.table = table;
+	}
+
+	/**
+	 * This function calculates the difference between the datevalues.
+	 * 
+	 * @return
+	 */
+	@Override
+	public Table compare() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
