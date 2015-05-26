060a3391ce9d4e43d41e57cddca27386ca3edc42
louisgosschalk <louis@dophoeve.nl>
2015-05-26 11:42:10 +0200

    aanpassing event

--- src/main/java/model/data/process/analysis/operations/comparisons/Comparison.java
+++ src/main/java/model/data/process/analysis/operations/comparisons/Comparison.java
@@ -4,19 +4,18 @@ import model.data.Table;
 import model.data.process.analysis.operations.Event;
 import model.data.process.analysis.operations.constraints.Constraint;
 
-
 /**
  * This class will initialize events and call its comparison.
  * 
  * @author Louis Gosschalk 21-05-2015
  */
 public abstract class Comparison {
-	
-	private Event event;
-	
+
+	protected Event event;
+
 	public Comparison(Table table, Constraint constraint) {
 		event = new Event(table, constraint);
 	}
-	
+
 	public abstract Table compare();
 }
