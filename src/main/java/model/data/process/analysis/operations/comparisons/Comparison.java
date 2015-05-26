f294c46cc23cd2f3fe2d220f04cebcde75d28d8d
louisgosschalk <louis@dophoeve.nl>
2015-05-26 10:52:39 +0200

    cleanup

--- src/main/java/model/data/process/analysis/operations/comparisons/Comparison.java
+++ src/main/java/model/data/process/analysis/operations/comparisons/Comparison.java
@@ -1,9 +1,6 @@
 package model.data.process.analysis.operations.comparisons;
 
-import model.data.Row;
 import model.data.process.analysis.ComparisonAnalysis;
-import model.data.process.analysis.operations.Operation;
-import model.data.value.NumberValue;
 
 /**
  * This class will initialize events and call its comparison.
@@ -11,9 +8,12 @@ import model.data.value.NumberValue;
  * @author Louis Gosschalk 21-05-2015
  */
 public class Comparison extends ComparisonAnalysis {
-
+	
+	private Comparison compare;
+	
 	public Comparison(Comparison comparison) {
 		super(comparison);
+		this.compare = comparison;
 		// TODO Auto-generated constructor stub
 	}
 
