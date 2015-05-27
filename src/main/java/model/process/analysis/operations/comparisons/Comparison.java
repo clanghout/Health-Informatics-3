aa73d06e4d5461f8d88c28fff5a933a48a617c42
louisgosschalk <louis@dophoeve.nl>
2015-05-27 11:12:41 +0200

    comparison todo

--- src/main/java/model/process/analysis/operations/comparisons/Comparison.java
+++ src/main/java/model/process/analysis/operations/comparisons/Comparison.java
@@ -4,7 +4,9 @@ import model.process.analysis.ComparisonAnalysis;
 
 /**
  * This class will initialize events and call its comparison.
- * 
+ * What is a comparison?
+ * 		- What are you comparing?
+ * 		- Based on what grounds? 
  * @author Louis Gosschalk 21-05-2015
  */
 public class Comparison extends ComparisonAnalysis {
