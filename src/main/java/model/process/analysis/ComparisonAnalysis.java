e724f935f3fc3d252119ce46517dd168ed28367e
Boudewijn <Boudewijn@vangroos.nl>
2015-05-27 11:45:14 +0200

    Fixed a lot of things being reported by CheckStyle, FindBugs and PMD.

--- src/main/java/model/process/analysis/ComparisonAnalysis.java
+++ src/main/java/model/process/analysis/ComparisonAnalysis.java
@@ -4,22 +4,25 @@ import model.data.Table;
 import model.process.analysis.operations.comparisons.Comparison;
 
 /**
- * Implements comparison analysis
+ * Implements comparison analysis.
  * 
  * @author Louis Gosschalk 26-05-2015
  */
 public class ComparisonAnalysis extends DataAnalysis {
 	
 	private final Comparison comparison;
-	
+
+	/**
+	 * Constructs a new ComparisonAnalysis.
+	 * @param comparison The comparison for this analysis.
+	 */
 	public ComparisonAnalysis(Comparison comparison) {
 		this.comparison = comparison;
 	}
 
 	@Override
 	public Table analyse(Table input) {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException("This code is not yet implemented");
 	} 
 	
 }
