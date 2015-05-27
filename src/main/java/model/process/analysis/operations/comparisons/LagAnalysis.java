7fe1de84a4adcb7e0473adfe29d366672fb83b64
louisgosschalk <louis@dophoeve.nl>
2015-05-27 11:15:47 +0200

    LagAnalysis todo explanation

--- src/main/java/model/process/analysis/operations/comparisons/LagAnalysis.java
+++ src/main/java/model/process/analysis/operations/comparisons/LagAnalysis.java
@@ -3,7 +3,11 @@ package model.process.analysis.operations.comparisons;
 /**
  * This class will calculate the lag between specified events.
  * 
- * @author Louis Gosschalk 21-05-2015
+ * the inputted table should be a combined table of two tables to compare
+ * two columns should be specified as measureDate and inputDate (datevalues)
+ * the difference between these dates is the lag.
+ * 
+ * @author Louis Gosschalk 26-05-2015
  */
 public class LagAnalysis {
 
