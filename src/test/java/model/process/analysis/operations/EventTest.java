2a9a55fda9d0547294caa3694d782a6cc1cb1961
louisgosschalk <louis@dophoeve.nl>
2015-06-01 12:04:16 +0200

    Afmaken event test

--- src/test/java/model/process/analysis/operations/EventTest.java
+++ src/test/java/model/process/analysis/operations/EventTest.java
@@ -1,16 +1,19 @@
 package model.process.analysis.operations;
 
 import static org.junit.Assert.*;
+
+import java.util.List;
+
 import model.data.DataColumn;
 import model.data.DataTable;
 import model.data.DataTableBuilder;
-import model.data.Table;
 import model.data.describer.ConstantDescriber;
 import model.data.describer.ConstraintDescriber;
 import model.data.describer.DataDescriber;
 import model.data.describer.RowValueDescriber;
 import model.data.value.BoolValue;
 import model.data.value.DateTimeValue;
+import model.data.value.IntValue;
 import model.data.value.StringValue;
 import model.process.analysis.operations.constraints.GreaterThanCheck;
 
@@ -19,14 +22,16 @@ import org.junit.Test;
 
 /**
  * This test creates an event.
- * 
+ *
  * @author Louis Gosschalk 28-05-2015
  */
-public abstract class EventTest {
+public class EventTest {
 
 	private DataTable table;
+	private DataTable table2;
 	private DataColumn stringColumn;
 	private DataColumn dateColumn;
+	private DataColumn measureColumn;
 	private DataTableBuilder builder;
 
 	/**
@@ -37,30 +42,59 @@ public abstract class EventTest {
 	@Before
 	public void setUp() {
 		builder = new DataTableBuilder();
+		builder.setName("test");
 		dateColumn = builder.createColumn("date", DateTimeValue.class);
 		stringColumn = builder.createColumn("string", StringValue.class);
+		measureColumn = builder.createColumn("measurement", IntValue.class);
+
 		DateTimeValue date = new DateTimeValue(19, 1, 1994, 11, 30, 5);
 		StringValue string = new StringValue("One");
-		builder.createRow(date, string);
+		IntValue inty = new IntValue(12);
+		builder.createRow(date, string, inty);
 
 		table = builder.build();
+
+		builder = new DataTableBuilder();
+		builder.setName("test");
+		dateColumn = builder.createColumn("date", DateTimeValue.class);
+		stringColumn = builder.createColumn("string", StringValue.class);
+		measureColumn = builder.createColumn("measurement", IntValue.class);
+
+		date = new DateTimeValue(19, 1, 1994, 11, 30, 5);
+		string = new StringValue("One");
+		inty = new IntValue(12);
+		builder.createRow(date, string, inty);
+		
+		date = new DateTimeValue(10, 1, 1997, 15, 20, 5);
+		string = new StringValue("Two");
+		inty = new IntValue(9);
+		builder.createRow(date, string, inty);
+
+		date = new DateTimeValue(9, 11, 2001, 14, 46, 28);
+		string = new StringValue("Three");
+		inty = new IntValue(10);
+		builder.createRow(date, string, inty);
+
+		table2 = builder.build();
 	}
 
 	/**
-	 * All instances of an event after a certain date.
+	 * Initialize an event (all measurements above 10).
 	 * 
 	 * @throws Exception
 	 */
 	@Test
 	public void testEvent() throws Exception {
-		assertEquals(1, 1);
-		DataDescriber<BoolValue> greater = new ConstraintDescriber(
-				new GreaterThanCheck<>(
-						new RowValueDescriber<>(table.getColumn("date")), 
-						new ConstantDescriber(new DateTimeValue(19, 1, 1993, 11, 30, 5))
-						));
-		Event event = new Event(table, greater);
-		Table tableResult = event.create();
-		assertEquals(table, tableResult);
+		assertFalse(table.equals(table2));
+		assertTrue(table.getRowCount() > 0);
+		assertTrue(table2.getRowCount() > 0);
+		DataDescriber<BoolValue> greater = 
+				new ConstraintDescriber(
+						new GreaterThanCheck<>(
+								new RowValueDescriber<>(table.getColumn("measurement")),
+								new ConstantDescriber<>(new IntValue(10))));
+		Event event = new Event(table2, greater);
+		DataTable tableResult = event.create();
+		assertTrue(table.equals(tableResult));
 	}
 }
