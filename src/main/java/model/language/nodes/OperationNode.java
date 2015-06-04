3cb9423e2cdf08fb1db35bf40afbb8da7ff1a0e4
Boudewijn <Boudewijn@vangroos.nl>
2015-06-04 11:30:47 +0200

    Added the OperationNode and TableValueNode.

---
+++ src/main/java/model/language/nodes/OperationNode.java
@@ -0,0 +1,23 @@
+package model.language.nodes;
+
+import model.data.value.DataValue;
+
+/**
+ * Represents an operation.
+ *
+ * Created by Boudewijn on 4-6-2015.
+ * @param <T> The type of DataValue this operation is applied to.
+ */
+public abstract class OperationNode<T extends DataValue> extends ValueNode {
+
+	private final String operation;
+
+	public OperationNode(ValueNode<T> left, String operation, ValueNode<T> right) {
+		super(left, right);
+		this.operation = operation;
+	}
+
+	public String getOperation() {
+		return operation;
+	}
+}
