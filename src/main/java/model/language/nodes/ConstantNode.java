b8a62581e3176d40b28d6d85713c7e157f2c90f2
Boudewijn <Boudewijn@vangroos.nl>
2015-06-04 11:35:48 +0200

    Implementing the ConstantNode.

--- src/main/java/model/language/nodes/ConstantNode.java
+++ src/main/java/model/language/nodes/ConstantNode.java
@@ -1,7 +1,36 @@
 package model.language.nodes;
 
+import model.data.DataModel;
+import model.data.describer.ConstantDescriber;
+import model.data.describer.DataDescriber;
+import model.data.value.DataValue;
+
 /**
+ * Represents a oonstant DataValue.
+ *
  * Created by Boudewijn on 4-6-2015.
+ * @param <T> The type of DataValue you want to contain in this node.
  */
-public class ConstantNode {
+public class ConstantNode<T extends DataValue> extends ValueNode<T> {
+
+	private final T constant;
+
+	/**
+	 * Construct a new ConstantNode.
+	 * @param constant The value you want to describe.
+	 */
+	public ConstantNode(T constant) {
+		super(null, null);
+		this.constant = constant;
+	}
+
+	/**
+	 * Returns a DataDescriber describing this constant.
+	 * @param model The model to be used.
+	 * @return A DataDescriber describing this constant.
+	 */
+	@Override
+	public DataDescriber<T> resolve(DataModel model) {
+		return new ConstantDescriber<>(constant);
+	}
 }
