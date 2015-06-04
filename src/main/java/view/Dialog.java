3c53ae47ca0b2d4ce18156c958e540992e021ca2
clanghout <chris.langhout@gmail.com>
2015-06-04 23:39:18 +0200

    fix errors in dialog.java

--- src/main/java/view/Dialog.java
+++ src/main/java/view/Dialog.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import java.io.IOException;
 
@@ -14,24 +16,29 @@ import java.io.IOException;
 public abstract class Dialog {
 	private Stage dialog;
 	private FXMLLoader fxml;
+	private Logger logger = Logger.getLogger("VisualizationController");
 
-	public Dialog (String location, String name) throws IOException {
-		fxml = new FXMLLoader(getClass().getResource(location));
+	public Dialog(String location, String name) throws IOException {
+		try {
+			fxml = new FXMLLoader(getClass().getResource(location));
+		} catch (Exception e) {
+			logger.log(Level.SEVERE, "resource not found");
+		}
 		Parent root = fxml.load();
 		dialog = new Stage();
 		dialog.setTitle(name);
 		dialog.setScene(new Scene(root));
 	}
 
-	public FXMLLoader getFxml () {
+	public FXMLLoader getFxml() {
 		return fxml;
 	}
 
-	public void show () {
+	public void show() {
 		dialog.show();
 	}
 
-	public void close () {
+	public void close() {
 		dialog.close();
 	}
 }
