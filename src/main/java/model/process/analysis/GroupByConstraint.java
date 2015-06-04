ce700df7ce1a4b5d94acd8a3102e3c20835c33fb
jenslangerak <J.Langerak@student.tudelft.nl>
2015-06-04 15:59:11 +0200

    test groupBy column

--- src/main/java/model/process/analysis/GroupByConstraint.java
+++ src/main/java/model/process/analysis/GroupByConstraint.java
@@ -1,8 +1,11 @@
 package model.process.analysis;
 
+import model.data.CombinedDataTable;
+import model.data.DataTable;
 import model.data.Table;
 import model.process.functions.Function;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 
 /**
@@ -12,6 +15,14 @@ import java.util.List;
  */
 public class GroupByConstraint extends GroupByAnalysis {
 
+	/**
+	 * Create a group by. The chunks are specified by constraints and the columns by functions.
+	 * @param name name of the new table
+	 * @param constrainList list of constrainst for the chunks
+	 * @param groupNames name for the chunks
+	 * @param functions functions for the columns
+	 * @param columnNames name for the columns
+	 */
 	public GroupByConstraint(String name, List<ConstraintAnalysis> constrainList,
 							 List<String> groupNames, List<Function> functions,
 							 List<String> columnNames) {
@@ -20,8 +31,30 @@ public class GroupByConstraint extends GroupByAnalysis {
 		constructBuilder(name, functions, columnNames);
 	}
 
+	/**
+	 * Construct the list of constraints for the chunks.
+	 * @param groupNames name of the chunks
+	 * @param constrainList constraints that specifies the chunks
+	 */
+	private void constructConstraintList(List<String> groupNames,
+										   List<ConstraintAnalysis> constrainList) {
+		if (constrainList.size() != groupNames.size()) {
+			throw new IllegalArgumentException("number of groups does not correspond "
+					+ "to the number of group names.");
+		}
+
+		LinkedHashMap<String, ConstraintAnalysis> constraints = new LinkedHashMap<>();
+		for (int i = 0; i < constrainList.size(); i++) {
+			constraints.put(groupNames.get(i), constrainList.get(i));
+		}
+		setConstraints(constraints);
+	}
+
 	@Override
 	public Table analyse(Table input) {
-		return groupBy(input);
+		if (input instanceof CombinedDataTable) {
+			throw new IllegalArgumentException("group by work only on datatable");
+		}
+		return groupBy((DataTable)input);
 	}
 }
