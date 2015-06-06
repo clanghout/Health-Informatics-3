ffe30b61e70865368261edaee6a8f90f98e4c61d
louisgosschalk <louis@dophoeve.nl>
2015-06-06 13:42:31 +0200

    additional testing

--- src/test/java/model/process/analysis/operations/comparisons/LagSequentialAnalysisTest.java
+++ src/test/java/model/process/analysis/operations/comparisons/LagSequentialAnalysisTest.java
@@ -1,6 +1,11 @@
 package model.process.analysis.operations.comparisons;
 
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import model.data.DataColumn;
 import model.data.DataTable;
 import model.data.DataTableBuilder;
@@ -30,8 +35,13 @@ public class LagSequentialAnalysisTest {
 	private Event event;
 	private Event event2;
 	private Event event3;
+	private Event event4;
+	private Event event5;
+	private DataTable tablecheck;
 	private DataDescriber<DateTimeValue> dateCol;
 	private DataDescriber<DateTimeValue> dateCol2;
+	private DataDescriber<DateTimeValue> dateCol4;
+	private DataDescriber<DateTimeValue> dateCol5;
 
 	/**
 	 * simulate two events.
@@ -121,6 +131,66 @@ public class LagSequentialAnalysisTest {
 						table3.getColumn("measurement")),
 						new ConstantDescriber<>(new IntValue(0))));
 		event3 = new Event(table3, greater3);
+		
+		DataTableBuilder builder4 = new DataTableBuilder();
+		builder4.setName("Tablecheck");
+		dat = builder4.createColumn("date", DateTimeValue.class);
+		builder4.createColumn("string", StringValue.class);
+		builder4.createColumn("measurement", IntValue.class);
+		
+		dateCol4 = new RowValueDescriber<DateTimeValue>(dat);
+		
+		date = new DateTimeValue(2015, 11, 9, 14, 46, 28);
+		string = new StringValue("Three");
+		inty = new IntValue(10);
+		builder4.createRow(date, string, inty);
+		
+		DataTable table4 = builder4.build();
+		
+		DataDescriber<BoolValue> greater4 = new ConstraintDescriber(
+				new GreaterThanCheck<>(new RowValueDescriber<>(
+						table4.getColumn("measurement")),
+						new ConstantDescriber<>(new IntValue(0))));
+		event4 = new Event(table4, greater4);
+		
+		DataTableBuilder builder5 = new DataTableBuilder();
+		builder5.setName("Tablecheck2");
+		dat = builder5.createColumn("date", DateTimeValue.class);
+		builder5.createColumn("string", StringValue.class);
+		builder5.createColumn("measurement", IntValue.class);
+		
+		date = new DateTimeValue(2015, 12, 9, 14, 46, 28);
+		string = new StringValue("Three");
+		inty = new IntValue(10);
+		builder5.createRow(date, string, inty);
+		
+		dateCol5 = new RowValueDescriber<DateTimeValue>(dat);
+		
+		DataTable table5 = builder5.build();
+		
+		DataDescriber<BoolValue> greater5 = new ConstraintDescriber(
+				new GreaterThanCheck<>(new RowValueDescriber<>(
+						table5.getColumn("measurement")),
+						new ConstantDescriber<>(new IntValue(0))));
+		event5 = new Event(table5, greater5);
+		
+		DataTableBuilder builder6 = new DataTableBuilder();
+		builder6.setName("Tablechecker");
+		dat = builder6.createColumn("date", DateTimeValue.class);
+		builder6.createColumn("string", StringValue.class);
+		builder6.createColumn("measurement", IntValue.class);
+		
+		date = new DateTimeValue(2015, 11, 9, 14, 46, 28);
+		string = new StringValue("Three");
+		inty = new IntValue(10);
+		builder6.createRow(date, string, inty);
+		
+		date = new DateTimeValue(2015, 12, 9, 14, 46, 28);
+		string = new StringValue("Three");
+		inty = new IntValue(10);
+		builder6.createRow(date, string, inty);
+		
+		tablecheck = builder6.build();
 
 	}
 
@@ -144,5 +214,22 @@ public class LagSequentialAnalysisTest {
 	public void testEmptyTable() throws Exception {
 		LagSequentialAnalysis lsa = new LagSequentialAnalysis(event3, dateCol,
 				event2, dateCol2);
+	}
+	
+	@Test
+	public void testGetTable() throws Exception {
+		LagSequentialAnalysis lsa = new LagSequentialAnalysis(event4, dateCol4,
+				event5, dateCol5);
+		DataTable result = lsa.getTable();
+		assertEquals(tablecheck.getRowCount(), result.getRowCount());
+	}
+	
+	@Test
+	public void testGetOrder() throws Exception {
+		LagSequentialAnalysis lsa = new LagSequentialAnalysis(event, dateCol,
+				event2, dateCol2);
+		List<String> order = lsa.getOrder();
+		List<String> ordercheck = Arrays.asList("A", "A", "B", "B", "A", "B", "A");
+		assertEquals(order,ordercheck);
 	}
 }
