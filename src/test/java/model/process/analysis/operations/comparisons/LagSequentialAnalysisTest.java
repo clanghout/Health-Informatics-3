7dd312afe1873ecff62e6af04d91c917288f2d11
louisgosschalk <louis@dophoeve.nl>
2015-06-06 12:45:06 +0200

    layout etc

--- src/test/java/model/process/analysis/operations/comparisons/LagSequentialAnalysisTest.java
+++ src/test/java/model/process/analysis/operations/comparisons/LagSequentialAnalysisTest.java
@@ -40,7 +40,7 @@ public class LagSequentialAnalysisTest {
 	 */
 	@Before
 	public void setUp() {
-		
+
 		DataTableBuilder builder = new DataTableBuilder();
 		builder.setName("test");
 		DataColumn dat = builder.createColumn("date", DateTimeValue.class);
@@ -51,47 +51,43 @@ public class LagSequentialAnalysisTest {
 		StringValue string = new StringValue("One");
 		IntValue inty = new IntValue(12);
 		builder.createRow(date, string, inty);
-		
+
 		date = new DateTimeValue(2014, 1, 19, 11, 30, 9);
 		string = new StringValue("One");
 		inty = new IntValue(12);
 		builder.createRow(date, string, inty);
-		
+
 		date = new DateTimeValue(1994, 1, 19, 11, 30, 9);
 		string = new StringValue("One");
 		inty = new IntValue(12);
 		builder.createRow(date, string, inty);
-		
+
 		date = new DateTimeValue(2016, 1, 19, 11, 30, 9);
 		string = new StringValue("One");
 		inty = new IntValue(12);
 		builder.createRow(date, string, inty);
-		
+
 		dateCol = new RowValueDescriber<DateTimeValue>(dat);
-		
+
 		DataTable table = builder.build();
 
-		DataDescriber<BoolValue> greater = 
-				new ConstraintDescriber(
-						new GreaterThanCheck<>(
-								new RowValueDescriber<>(table.getColumn("measurement")),
-								new ConstantDescriber<>(new IntValue(0))
-						)
-				);
+		DataDescriber<BoolValue> greater = new ConstraintDescriber(
+				new GreaterThanCheck<>(new RowValueDescriber<>(
+						table.getColumn("measurement")),
+						new ConstantDescriber<>(new IntValue(0))));
 		event = new Event(table, greater);
-		
-		
+
 		DataTableBuilder builder2 = new DataTableBuilder();
 		builder2.setName("test2");
 		dat = builder2.createColumn("date", DateTimeValue.class);
 		builder2.createColumn("string", StringValue.class);
 		builder2.createColumn("measurement", IntValue.class);
-		
+
 		date = new DateTimeValue(2015, 1, 19, 11, 30, 5);
 		string = new StringValue("One");
 		inty = new IntValue(12);
 		builder2.createRow(date, string, inty);
-		
+
 		date = new DateTimeValue(2014, 1, 20, 15, 20, 5);
 		string = new StringValue("Two");
 		inty = new IntValue(9);
@@ -101,37 +97,31 @@ public class LagSequentialAnalysisTest {
 		string = new StringValue("Three");
 		inty = new IntValue(10);
 		builder2.createRow(date, string, inty);
-		
+
 		dateCol2 = new RowValueDescriber<DateTimeValue>(dat);
-		
+
 		DataTable table2 = builder2.build();
-		
-		DataDescriber<BoolValue> greater2 = 
-				new ConstraintDescriber(
-						new GreaterThanCheck<>(
-								new RowValueDescriber<>(table2.getColumn("measurement")),
-								new ConstantDescriber<>(new IntValue(0))
-						)
-				);
+
+		DataDescriber<BoolValue> greater2 = new ConstraintDescriber(
+				new GreaterThanCheck<>(new RowValueDescriber<>(
+						table2.getColumn("measurement")),
+						new ConstantDescriber<>(new IntValue(0))));
 		event2 = new Event(table2, greater2);
-		
+
 		DataTableBuilder builder3 = new DataTableBuilder();
 		builder3.setName("EMPTYTABLETEST");
 		dat = builder3.createColumn("date", DateTimeValue.class);
 		builder3.createColumn("string", StringValue.class);
 		builder3.createColumn("measurement", IntValue.class);
-		
+
 		DataTable table3 = builder3.build();
-		
-		DataDescriber<BoolValue> greater3 = 
-				new ConstraintDescriber(
-						new GreaterThanCheck<>(
-								new RowValueDescriber<>(table3.getColumn("measurement")),
-								new ConstantDescriber<>(new IntValue(0))
-						)
-				);
+
+		DataDescriber<BoolValue> greater3 = new ConstraintDescriber(
+				new GreaterThanCheck<>(new RowValueDescriber<>(
+						table3.getColumn("measurement")),
+						new ConstantDescriber<>(new IntValue(0))));
 		event3 = new Event(table3, greater3);
-		
+
 	}
 
 	/**
@@ -141,11 +131,18 @@ public class LagSequentialAnalysisTest {
 	 */
 	@Test
 	public void testEvent() throws Exception {
-		LagSequentialAnalysis lsa = new LagSequentialAnalysis(event, dateCol, event2, dateCol2);
+		LagSequentialAnalysis lsa = new LagSequentialAnalysis(event, dateCol,
+				event2, dateCol2);
 	}
-	
+
+	/**
+	 * Call LSA on empty table event.
+	 * 
+	 * @throws Exception
+	 */
 	@Test(expected = InputMismatchException.class)
 	public void testEmptyTable() throws Exception {
-		LagSequentialAnalysis lsa = new LagSequentialAnalysis(event3, dateCol, event2, dateCol2);
+		LagSequentialAnalysis lsa = new LagSequentialAnalysis(event3, dateCol,
+				event2, dateCol2);
 	}
 }
