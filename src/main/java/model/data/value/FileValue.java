4db42c26697e097ccae1e4fbf1ef563ae2a600b7
Paul <P.J.vanWijk@student.tudelft.nl>
2015-06-06 17:00:03 +0000

    update filevalue

--- src/main/java/model/data/value/FileValue.java
+++ src/main/java/model/data/value/FileValue.java
@@ -3,38 +3,40 @@ package model.data.value;
 import model.input.file.DataFile;
 
 /**
+ * Class to represent a file as metadata.
+ *
  * @author Paul
  */
 public class FileValue extends DataValue {
 
-    private DataFile file;
+	private DataFile file;
 
-    public FileValue(DataFile file) {
-        this.file = file;
-    }
+	public FileValue(DataFile file) {
+		this.file = file;
+	}
 
-    @Override
-    public Object getValue() {
-        return this.file;
-    }
+	@Override
+	public Object getValue() {
+		return this.file;
+	}
 
-    @Override
-    public String toString() {
-        return file.toString();
-    }
+	@Override
+	public String toString() {
+		return file.getPath();
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        return obj instanceof FileValue && ((FileValue) obj).getValue().equals(this.getValue());
-    }
+	@Override
+	public boolean equals(Object obj) {
+		return obj instanceof FileValue && ((FileValue) obj).getValue().equals(this.getValue());
+	}
 
-    @Override
-    public int hashCode() {
-        return file.hashCode();
-    }
+	@Override
+	public int hashCode() {
+		return file.hashCode();
+	}
 
-    @Override
-    public DataValue copy() {
-        return new FileValue(this.file);
-    }
+	@Override
+	public DataValue copy() {
+		return new FileValue(this.file);
+	}
 }
