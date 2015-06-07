d809c8eb59cee229fb3d9eb23796f6810d10a814
clanghout <chris.langhout@gmail.com>
2015-06-07 22:23:43 +0200

    make popup larger so error messages fit

--- src/main/java/controllers/visualizations/ChartController.java
+++ src/main/java/controllers/visualizations/ChartController.java
@@ -2,8 +2,13 @@ package controllers.visualizations;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.Chart;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.util.StringConverter;
 import model.data.DataColumn;
 import model.data.DataTable;
@@ -13,6 +18,23 @@ import model.data.DataTable;
  * Created by Chris on 4-6-2015.
  */
 public abstract class ChartController {
+	protected DataTable table;
+	protected CategoryAxis xAxis;
+	protected NumberAxis yAxis;
+	protected VBox vBox;
+	protected DataColumn xCol;
+	protected DataColumn yCol;
+	protected boolean xSet = false;
+	protected boolean ySet = false;
+	protected ComboBox<DataColumn> xAxisBox;
+	protected ComboBox<DataColumn> yAxisBox;
+	protected Label xAxisErrorLabel;
+	protected Label yAxisErrorLabel;
+
+	public ChartController(DataTable table, VBox vBox) {
+		this.table = table;
+		this.vBox = vBox;
+	}
 
 	/**
 	 * Creation of the controller.
@@ -24,13 +46,15 @@ public abstract class ChartController {
 	 * Used to check before building the actual graph.
 	 * @return true if all axes are well defined.
 	 */
-	public abstract boolean axesSet();
+	public boolean axesSet() {
+		return xSet && ySet;
+	}
 
 	/**
 	 * Create the actual javaFX element
 	 * @return JavaFX chart with valid data.
 	 */
-	public abstract Chart create();
+	public abstract javafx.scene.chart.Chart create();
 
 	/**
 	 * Set the items of a comboBox to the columns of the dataTable.
@@ -55,4 +79,33 @@ public abstract class ChartController {
 			}
 		});
 	}
+
+	/**
+	 * Set the message to an error label.
+	 *
+	 * @param label   the label wich will show the error.
+	 * @param message the message for in the label.
+	 */
+	public void setErrorLabel(Label label, String message) {
+		label.setTextFill(Color.RED);
+		label.setText(message);
+	}
+
+	/**
+	 * Initialize the fxml objects for ChartController.
+	 */
+	public void initializeFields() {
+		xAxisBox = new ComboBox<>();
+		xAxisBox.setPromptText("x-Axis");
+		xAxisErrorLabel = new Label();
+		xAxisErrorLabel.setMaxWidth(Double.MAX_VALUE);
+		yAxisBox = new ComboBox<>();
+		yAxisBox.setPromptText("y-Axis");
+		yAxisErrorLabel = new Label();
+		yAxisErrorLabel.setMaxWidth(Double.MAX_VALUE);
+		setColumnDropDown(xAxisBox, table);
+		setColumnDropDown(yAxisBox, table);
+	}
+
+
 }
