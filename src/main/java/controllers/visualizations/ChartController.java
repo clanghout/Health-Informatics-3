fadb34950333ec34f4189b618149e763703ec6b6
clanghout <chris.langhout@gmail.com>
2015-06-07 13:08:19 +0200

    add javadoc for chartcontoller

--- src/main/java/controllers/visualizations/ChartController.java
+++ src/main/java/controllers/visualizations/ChartController.java
@@ -9,13 +9,29 @@ import model.data.DataColumn;
 import model.data.DataTable;
 
 /**
+ * Abstract ChartController specifying the controller for a javaFX chart.
  * Created by Chris on 4-6-2015.
  */
 public abstract class ChartController {
 
+	/**
+	 * Creation of the controller.
+	 */
 	public abstract void initialize();
+
+	/**
+	 * Checks if all data Axes are set and contain valid information.
+	 * Used to check before building the actual graph.
+	 * @return true if all axes are well defined.
+	 */
 	public abstract boolean axesSet();
+
+	/**
+	 * Create the actual javaFX element
+	 * @return JavaFX chart with valid data.
+	 */
 	public abstract Chart create();
+
 	/**
 	 * Set the items of a comboBox to the columns of the dataTable.
 	 *
