b9dc3c7ed1f8e6245a15a80eafa35f80016ff0f2
Boudewijn <Boudewijn@vangroos.nl>
2015-06-09 11:53:44 +0200

    Added the DateConstraintDescriber.

---
+++ src/main/java/model/data/describer/DateConstraintDescriber.java
@@ -0,0 +1,25 @@
+package model.data.describer;
+
+import model.data.Row;
+import model.data.value.BoolValue;
+import model.data.value.TemporalValue;
+import model.process.analysis.operations.dates.constraint.DateConstraint;
+
+/**
+ * Describes a DateConstraint.
+ *
+ * Created by Boudewijn on 9-6-2015.
+ */
+public class DateConstraintDescriber extends DataDescriber<BoolValue> {
+
+	private DateConstraint constraint;
+
+	public DateConstraintDescriber(DateConstraint constraint) {
+		this.constraint = constraint;
+	}
+
+	@Override
+	public BoolValue resolve(Row row) {
+		return constraint.operate(row);
+	}
+}
