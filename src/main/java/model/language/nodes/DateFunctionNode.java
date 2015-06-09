8eb284fa6e355323f364471ec30b114a52772316
Boudewijn <boudewijn@vangroos.nl>
2015-06-09 22:27:26 +0200

    Added JavaDoc to DateFunctionNode.

--- src/main/java/model/language/nodes/DateFunctionNode.java
+++ src/main/java/model/language/nodes/DateFunctionNode.java
@@ -10,6 +10,8 @@ import model.process.analysis.operations.dates.computations.Relative;
 import java.time.temporal.ChronoUnit;
 
 /**
+ * Represents a function performed on 2 dates and a unit.
+ *
  * Created by Boudewijn on 9-6-2015.
  */
 public class DateFunctionNode extends ValueNode<IntValue> {
@@ -18,6 +20,12 @@ public class DateFunctionNode extends ValueNode<IntValue> {
 	private ValueNode<TemporalValue<?>> right;
 	private String unit;
 
+	/**
+	 * Construct a new DateFunctionNode.
+	 * @param left The first argument of this function.
+	 * @param right The second argument of this function.
+	 * @param unit The unit this function should output.
+	 */
 	public DateFunctionNode(
 			ValueNode<? extends TemporalValue<?>> left,
 			ValueNode<TemporalValue<?>> right,
