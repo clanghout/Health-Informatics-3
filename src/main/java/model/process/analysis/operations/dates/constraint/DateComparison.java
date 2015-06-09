e92b1af7b5891ff8f7dc1529f802621b6132f13a
Boudewijn <boudewijn@vangroos.nl>
2015-06-09 14:53:32 +0200

    Dates are now more interoperable.

--- src/main/java/model/process/analysis/operations/dates/constraint/DateComparison.java
+++ src/main/java/model/process/analysis/operations/dates/constraint/DateComparison.java
@@ -4,6 +4,8 @@ import model.data.Row;
 import model.data.describer.DataDescriber;
 import model.data.value.TemporalValue;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.time.temporal.Temporal;
 
@@ -34,8 +36,47 @@ public abstract class DateComparison extends DateConstraint {
 	protected boolean check(Row row) {
 		Temporal leftValue = getLeft().resolve(row).getValue();
 		Temporal rightValue = getRight().resolve(row).getValue();
-		long amount = ChronoUnit.SECONDS.between(leftValue, rightValue);
+
+
+		long amount = getAmount(leftValue, rightValue);
 		return compare(amount);
+	}
+
+	private long getAmount(Temporal left, Temporal right) {
+		if (left.getClass() == right.getClass()) {
+			if (eitherDate(left, right)) {
+				return ChronoUnit.DAYS.between(left, right);
+			} else {
+				return ChronoUnit.SECONDS.between(left, right);
+			}
+		} else {
+			if (eitherDate(left, right)) {
+				if (eitherDateTime(left, right)) {
+					LocalDateTime leftDateTime = convertToDateTime(left);
+					LocalDateTime rightDateTime = convertToDateTime(right);
+					return ChronoUnit.SECONDS.between(leftDateTime, rightDateTime);
+				}
+			}
+		}
+		throw new UnsupportedOperationException("Time can't be converted to DateTime");
+	}
+
+	private boolean eitherDate(Temporal left, Temporal right) {
+		return left instanceof LocalDate || right instanceof LocalDate;
+	}
+
+	private boolean eitherDateTime(Temporal left, Temporal right) {
+		return left instanceof LocalDateTime || right instanceof LocalDateTime;
+	}
+
+	private LocalDateTime convertToDateTime(Temporal value) {
+		if (value instanceof LocalDateTime) {
+			return (LocalDateTime) value;
+		} else if (value instanceof LocalDate) {
+			return ((LocalDate) value).atStartOfDay();
+		} else {
+			throw new RuntimeException("This shouldn't happen");
+		}
 	}
 
 	/**
