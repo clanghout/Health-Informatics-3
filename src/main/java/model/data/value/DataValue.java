330e8b538e5734ff9f4cca260dd92ee6abb781e0
louisgosschalk <louis@dophoeve.nl>
2015-06-10 09:21:15 +0200

    JavaDoc

--- src/main/java/model/data/value/DataValue.java
+++ src/main/java/model/data/value/DataValue.java
@@ -1,14 +1,16 @@
 package model.data.value;
 
-
 /**
  * Abstract class DataValue describing the DataValue objects.
- * @param <Type> return type of getValue()
+ * 
+ * @param <Type>
+ *            return type of getValue()
  */
 public abstract class DataValue<Type> {
 	private boolean isNull;
-	
+
 	public abstract Type getValue();
+
 	public abstract String toString();
 	
 	@Override
@@ -27,14 +29,33 @@ public abstract class DataValue<Type> {
 
 	@Override
 	public abstract int hashCode();
-	
+
+	/**
+	 * Shows true if the value of the object is Null.
+	 * 
+	 * @return isNull
+	 */
 	public boolean isNull() {
 		return isNull;
 	}
-	
+
+	/**
+	 * Set true if value of the object is null.
+	 * 
+	 * @param val
+	 *            false if value != null
+	 */
 	protected void setNull(boolean val) {
 		isNull = val;
 	}
-	
+
+	/**
+	 * Additional function for equals for specific characteristics of each
+	 * DataValue.
+	 * 
+	 * @param obj
+	 *            The object to compare
+	 * @return true if object value equals this value (null != null)
+	 */
 	protected abstract boolean doEquals(Object obj);
 }
