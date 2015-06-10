e722cec593d06468869fde502f84b85ffcefe7be
jenslangerak <J.Langerak@student.tudelft.nl>
2015-06-10 10:07:31 +0200

    add the simple join

--- src/main/java/model/process/setOperations/SimpleJoin.java
+++ src/main/java/model/process/setOperations/SimpleJoin.java
@@ -1,7 +1,38 @@
 package model.process.setOperations;
 
+import model.data.*;
+import model.language.Identifier;
+
+import java.util.Map;
+
 /**
  * Created by jens on 6/10/15.
  */
-public class SimpleJoin {
+public class SimpleJoin extends Join {
+	private  Identifier<DataTable> table;
+
+	public SimpleJoin(String name,  Identifier<DataTable> table) {
+		super(name);
+		this.table = table;
+	}
+
+	@Override
+	protected void joinTable() {
+		for (Row row : (Iterable<Row>) table) {
+			DataRow newRow = new DataRow();
+			if (getConstraint().resolve(row).getValue()) {
+				for (Map.Entry<DataColumn, DataColumn> entry : getMappingColumns().entrySet()) {
+					checkValue(newRow, entry.getValue(), row.getValue(entry.getKey()));
+				}
+				newRow.addCodes(row.getCodes());
+
+				getBuilder().addRow(newRow);
+			}
+		}
+	}
+
+	@Override
+	protected Table getTable() {
+		return getDataModel().getByName(table.getName()).get();
+	}
 }
