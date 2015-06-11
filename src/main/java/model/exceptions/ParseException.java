58a090593d6dde9a89ce2966268ce01bda8381ea
Boudewijn <Boudewijn@vangroos.nl>
2015-06-11 11:00:03 +0200

    Implemented the ParseException.

--- src/main/java/model/exceptions/ParseException.java
+++ src/main/java/model/exceptions/ParseException.java
@@ -1,7 +1,29 @@
 package model.exceptions;
 
+import org.parboiled.errors.ParseError;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created by Boudewijn on 11-6-2015.
  */
-public class ParseException {
+public class ParseException extends Exception {
+
+	private final List<ParseError> errors;
+
+	public ParseException(String message) {
+		super(message);
+		errors = Collections.emptyList();
+	}
+
+	public ParseException(String message, List<ParseError> errors) {
+		super(message);
+		this.errors = new ArrayList<>(errors);
+	}
+
+	public List<ParseError> getParseErrors() {
+		return Collections.unmodifiableList(errors);
+	}
 }
