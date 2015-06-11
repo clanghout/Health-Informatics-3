4f41c6395a0fb96d21f74f07f0a025521c1c6c4b
Boudewijn <boudewijn@vangroos.nl>
2015-06-11 15:52:48 +0200

    Removed useless import.

--- src/main/java/model/language/LanguageParser.java
+++ src/main/java/model/language/LanguageParser.java
@@ -8,7 +8,6 @@ import org.parboiled.Context;
 import org.parboiled.Rule;
 import org.parboiled.support.Var;
 
-import javax.swing.text.html.Option;
 import java.util.ArrayList;
 import java.util.List;
 
