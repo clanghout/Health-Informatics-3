6f90c19293513453e03df8e0529f29ebe5c181e0
louisgosschalk <louis@dophoeve.nl>
2015-06-12 12:05:32 +0200

    DataAnalyse additie voor parser

--- src/main/java/model/process/analysis/LagSequentialAnalysis.java
+++ src/main/java/model/process/analysis/LagSequentialAnalysis.java
@@ -1,6 +1,10 @@
 package model.process.analysis;
 
+import model.data.DataColumn;
 import model.data.Table;
+import model.language.Identifier;
+import model.process.analysis.operations.Event;
+import model.process.analysis.operations.comparisons.LagSequential;
 
 /**
  * Implements the Lag Sequential analysis.
@@ -8,23 +12,32 @@ import model.data.Table;
  * @author Louis Gosschalk 11-06-2015
  */
 public class LagSequentialAnalysis extends DataAnalysis {
-
+	private Event eventA;
+	private Event eventB;
+	private Identifier<DataColumn> dateA;
+	private Identifier<DataColumn> dateB;
 
 	/**
-	 * Construct a new LagSequentialAnalysis.
-	 * @param constraint The constraint you want to use for this analysis.
+	 * Construct a new Lag Sequential analysis.
+	 * 
+	 * @param eventA
+	 * @param dateA
+	 * @param eventB
+	 * @param dateB
 	 */
-	public LagSequentialAnalysis() {
-		/**
-		 * TODO build this
-		 */
+	public LagSequentialAnalysis(Event eventA, Identifier<DataColumn> dateA,
+			Event eventB, Identifier<DataColumn> dateB) {
+		this.eventA = eventA;
+		this.eventB = eventB;
+		this.dateA = dateA;
+		this.dateB = dateB;
 	}
 
 	@Override
 	public Table analyse(Table input) {
-		/**
-		 * TODO build this too
-		 */
+		LagSequential lsa = new LagSequential(eventA, dateA, eventB,
+				dateB);
+		input = lsa.getResult();
 		return input;
 	}
 }
