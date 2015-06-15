931597c6832a882320723544ea6903dce67c96b2
louisgosschalk <louis@dophoeve.nl>
2015-06-15 10:36:25 +0200

    test auto layout

--- src/test/java/model/process/analysis/operations/comparisons/TimeBetweenTest.java
+++ src/test/java/model/process/analysis/operations/comparisons/TimeBetweenTest.java
@@ -18,100 +18,109 @@ import org.junit.Test;
 
 public class TimeBetweenTest {
 	private DataTable table;
-	
+
 	@Before
 	public void setUp() {
 		DataTableBuilder builder = new DataTableBuilder();
 		builder.setName("test table");
-		
+
 		builder.createColumn("date time", DateTimeValue.class);
 		builder.createColumn("date", DateValue.class);
 		builder.createColumn("time", TimeValue.class);
-		
+
 		DataValue datetime = new DateTimeValue(2014, 6, 20, 10, 20, 10);
 		DataValue date = new DateValue(2019, 6, 9);
 		DataValue time = new TimeValue(21, 12, 50);
 		builder.createRow(datetime, date, time);
-		
+
 		datetime = new DateTimeValue(2016, 1, 19, 10, 10, 10);
 		date = new DateValue(2019, 7, 4);
 		time = new TimeValue(20, 8, 29);
 		builder.createRow(datetime, date, time);
-		
+
 		table = builder.build();
-		
+
 	}
-	
+
 	@Test
 	public void testDateTime() {
-		Identifier<DataColumn> idDateTime = new Identifier<DataColumn>("date time");
-		
+		Identifier<DataColumn> idDateTime = new Identifier<DataColumn>(
+				"date time");
+
 		DataProcess tb = new TimeBetween(idDateTime);
 		tb.setInput(table);
-		
+
 		DataTable result = (DataTable) tb.process();
-		
+
 		assertEquals(result.getRowCount(), 2);
-		
+
 		DataValue compareDate = new PeriodValue(0, 0, 0);
-		DataValue dateDiff = result.getRow(0).getValue(result.getColumn("Difference date"));
+		DataValue dateDiff = result.getRow(0).getValue(
+				result.getColumn("Difference date"));
 		DataValue compareTime = new TimeValue(0, 0, 0);
-		DataValue timeDiff = result.getRow(0).getValue(result.getColumn("Difference time"));
-		
+		DataValue timeDiff = result.getRow(0).getValue(
+				result.getColumn("Difference time"));
+
 		assertEquals(compareDate, dateDiff);
 		assertEquals(compareTime, timeDiff);
-		
+
 		compareDate = new PeriodValue(1, 6, 29);
-		dateDiff = result.getRow(1).getValue(result.getColumn("Difference date"));
+		dateDiff = result.getRow(1).getValue(
+				result.getColumn("Difference date"));
 		compareTime = new TimeValue(23, 50, 0);
-		timeDiff = result.getRow(1).getValue(result.getColumn("Difference time"));
-		
+		timeDiff = result.getRow(1).getValue(
+				result.getColumn("Difference time"));
+
 		assertEquals(compareDate, dateDiff);
 		assertEquals(compareTime, timeDiff);
 	}
-	
+
 	@Test
 	public void testDate() {
 		Identifier<DataColumn> idDate = new Identifier<DataColumn>("date");
-		
+
 		DataProcess tb = new TimeBetween(idDate);
 		tb.setInput(table);
-		
+
 		DataTable result = (DataTable) tb.process();
-		
+
 		assertEquals(result.getRowCount(), 2);
-		
+
 		DataValue compareDate = new PeriodValue(0, 0, 0);
-		DataValue dateDiff = result.getRow(0).getValue(result.getColumn("Difference date"));
-		
+		DataValue dateDiff = result.getRow(0).getValue(
+				result.getColumn("Difference date"));
+
 		assertEquals(compareDate, dateDiff);
-		
+
 		compareDate = new PeriodValue(0, 0, 25);
-		dateDiff = result.getRow(1).getValue(result.getColumn("Difference date"));
-		
+		dateDiff = result.getRow(1).getValue(
+				result.getColumn("Difference date"));
+
 		assertEquals(compareDate, dateDiff);
 	}
-	
+
 	@Test
 	public void testTime() {
 		Identifier<DataColumn> idTime = new Identifier<DataColumn>("time");
-		
+
 		DataProcess tb = new TimeBetween(idTime);
 		tb.setInput(table);
-		
+
 		DataTable result = (DataTable) tb.process();
-		
+
 		assertEquals(result.getRowCount(), 2);
-		
+
 		DataValue compareTime = new TimeValue(0, 0, 0);
-		DataValue timeDiff = result.getRow(0).getValue(result.getColumn("Difference time"));
-		
+		DataValue timeDiff = result.getRow(0).getValue(
+				result.getColumn("Difference time"));
+
 		assertEquals(compareTime, timeDiff);
-		
+
 		compareTime = new TimeValue(22, 55, 39);
-		timeDiff = result.getRow(1).getValue(result.getColumn("Difference time"));
-		
+		timeDiff = result.getRow(1).getValue(
+				result.getColumn("Difference time"));
+
 		assertEquals(compareTime, timeDiff);
 	}
-	
+
 }
