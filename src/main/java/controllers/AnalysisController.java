e2c14624be8b64b4a9a37c3bf5ededb965a9a2c4
clanghout <chris.langhout@gmail.com>
2015-06-16 12:24:14 +0200

    Add better errors for parseExceptions

--- src/main/java/controllers/AnalysisController.java
+++ src/main/java/controllers/AnalysisController.java
@@ -4,15 +4,20 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.VBox;
 import model.exceptions.ParseException;
 import model.language.Parser;
 import model.data.DataModel;
 import model.process.DataProcess;
+import org.parboiled.buffers.InputBuffer;
+import org.parboiled.errors.ParseError;
+
+import java.util.List;
 
 
 /**
  * The controller for the Analysis tab.
- *
+ * <p>
  * Created by Boudewijn on 22-5-2015.
  */
 public class AnalysisController {
@@ -22,10 +27,15 @@ public class AnalysisController {
 	@FXML
 	private TextArea userscript;
 	@FXML
-	private Label errorLabel, errorLabelExtra;
+	private Label errorLabel;
+	@FXML
+	private VBox errorBox;
+
+	private static final int ERROR_RANGE = 5;
 
 	/**
 	 * Sets the model that will be observed.
+	 *
 	 * @param model The model
 	 */
 	public void setDataModel(DataModel model) {
@@ -36,28 +46,54 @@ public class AnalysisController {
 	protected void handleExecuteButtonAction(ActionEvent event) {
 		Parser parser = new Parser();
 		emptyLabels();
-
+		Label errorLabelExtra = new Label();
 		try {
 			DataProcess process = parser.parse(userscript.getText(), model);
 			process.process();
 			model.setUpdated();
+
 		} catch (IllegalArgumentException e) {
 			errorLabel.setText("ERROR: " + e.getMessage());
-			errorLabelExtra.setText(e.toString());
 		} catch (ParseException e) {
-			errorLabel.setText("");
-			errorLabelExtra.setText(e.toString());
+			errorLabel.setText(e.getMessage());
+			createParseExceptionMessage(e);
 		} catch (UnsupportedOperationException e) {
 			errorLabel.setText("ERROR: you are using an invalid operation");
-			errorLabelExtra.setText(e.toString());
 		} catch (RuntimeException e) {
-			errorLabel.setText("Runtime exception occured");
-			errorLabelExtra.setText(e.toString());
+			errorLabel.setText("Runtime exception occurred");
+			errorLabelExtra.setText(e.getLocalizedMessage() + " | " + e.getMessage());
+		}
+		errorBox.getChildren().add(errorLabelExtra);
+	}
+
+	/**
+	 * Create a label with error message for every parse error.
+	 * @param e teh Exception containing the parse errors
+	 */
+	private void createParseExceptionMessage(ParseException e) {
+		List<ParseError> parseErrors = e.getParseErrors();
+		for (ParseError error : parseErrors) {
+			InputBuffer buffer = error.getInputBuffer();
+			Label label = new Label();
+			label.setMaxWidth(Double.MAX_VALUE);
+			String beforeError = buffer.extract(0, error.getStartIndex());
+			int lineCount = beforeError.length() - beforeError.replace("\n", "").length();
+			String errorChar =
+					buffer.extract(error.getStartIndex() - ERROR_RANGE,
+							error.getEndIndex() + ERROR_RANGE);
+			label.setText("Error at line "
+					+ lineCount
+					+ "; near \""
+					+ errorChar + "\"");
+			errorBox.getChildren().add(label);
 		}
 	}
 
+	/**
+	 * Clear all the error labels.
+	 */
 	private void emptyLabels() {
 		errorLabel.setText("");
-		errorLabelExtra.setText("");
+		errorBox.getChildren().clear();
 	}
 }
